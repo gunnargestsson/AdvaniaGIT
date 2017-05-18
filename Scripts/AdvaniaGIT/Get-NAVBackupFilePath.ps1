a8efc3d911fbcb292f3cc3eacdcc518af4190782
Gunnar Gestsson <gg@advania.is>
2017-05-18 10:14:07 +0000

    Smarter way of picking up a backup

--- Scripts/AdvaniaGIT/Get-NAVBackupFilePath.ps1
+++ Scripts/AdvaniaGIT/Get-NAVBackupFilePath.ps1
@@ -9,36 +9,34 @@
         [PSObject]$BranchSettings
     )
     $TempBackupFilePath = (Join-Path $LogPath "NAVBackup.bak")
-    $BackupFilePath = (Join-Path $BackupPath ("$($SetupParameters.navRelease)-$($SetupParameters.projectName).bak"))
-    if (Test-Path $BackupFilePath) {
-        Copy-Item -Path $BackupFilePath -Destination $TempBackupFilePath -Force
-    } else {
-        $BackupFilePath = (Join-Path $BackupPath ("$($SetupParameters.navRelease)-$($SetupParameters.navSolution).bak"))
-        if (Test-Path $BackupFilePath) {
-            Copy-Item -Path $BackupFilePath -Destination $TempBackupFilePath -Force
-        } else {
-            $FtpDirectory = Get-FtpDirectory -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -Directory "$($SetupParameters.navRelease)/"
-            $FtpFiles = @(
-              "$($SetupParameters.navRelease)/$($SetupParameters.navVersion)/$($SetupParameters.projectName).bak",
-              "$($SetupParameters.navRelease)/$($SetupParameters.projectName).bak",
-              "$($SetupParameters.navRelease)/$($SetupParameters.navVersion)/$($SetupParameters.navSolution).bak",
-              "$($SetupParameters.navRelease)/$($SetupParameters.navSolution).bak")
-            foreach ($FtpFile in $FtpFiles) {
-                if ($FtpDirectory -imatch $FtpFile ) {
-                    Get-FtpFile `
-                        -Server $SetupParameters.ftpServer `
-                        -User $SetupParameters.ftpUser `
-                        -Pass $SetupParameters.ftpPass `
-                        -FtpFilePath $FtpFile `
-                        -LocalFilePath $TempBackupFilePath
-                    break
-                }
+    $Backups = @()
+    $Backups += (Get-ChildItem -Path $BackupPath -File).Name
+    $Backups += Get-FtpDirectory -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -Directory "$($SetupParameters.navRelease)/"
+    $FilePatterns = @(
+        "$($SetupParameters.navRelease)-$($SetupParameters.projectName).bak",
+        "$($SetupParameters.navRelease)/$($SetupParameters.navVersion)/$($SetupParameters.projectName).bak",
+        "$($SetupParameters.navRelease)/$($SetupParameters.projectName).bak",
+        "$($SetupParameters.navRelease)-$($SetupParameters.navSolution).bak"
+        "$($SetupParameters.navRelease)/$($SetupParameters.navVersion)/$($SetupParameters.navSolution).bak",
+        "$($SetupParameters.navRelease)/$($SetupParameters.navSolution).bak")
+    foreach ($FilePattern in $FilePatterns) {
+        if ($Backups -imatch $FilePattern ) {
+            if (Test-Path (Join-Path $BackupPath $FilePattern)) {
+                Copy-Item -Path (Join-Path $BackupPath $FilePattern) -Destination $TempBackupFilePath -Force
+            } else {
+                Get-FtpFile `
+                    -Server $SetupParameters.ftpServer `
+                    -User $SetupParameters.ftpUser `
+                    -Pass $SetupParameters.ftpPass `
+                    -FtpFilePath $FilePattern `
+                    -LocalFilePath $TempBackupFilePath
             }
+            break
         }
     }
     if (Test-Path $TempBackupFilePath) {
         return $TempBackupFilePath
     } else {
-        Write-Error "No backup found for $($SetupParameters.navRelease)/$($SetupParameters.navVersion)/$($SetupParameters.navSolution)" -ErrorAction Stop
+        Write-Error "No backup found for $($SetupParameters.projectName)" -ErrorAction Stop
     }
 }
