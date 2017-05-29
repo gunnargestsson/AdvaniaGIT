dd7e83acc5e5ee15f04e5f8caccdc69179e84ba5
Gunnar Gestsson <gg@advania.is>
2017-05-29 17:38:35 +0000

    More cases where I need to handle json files as string before converting

--- Scripts/AdvaniaGIT/Delete-BranchSettings.ps1
+++ Scripts/AdvaniaGIT/Delete-BranchSettings.ps1
@@ -6,7 +6,7 @@
         [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
         [String]$SettingsFilePath = "Data\BranchSettings.Json"
     )
-    $allBranchSettings = Get-Content -Path (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) $SettingsFilePath) | ConvertFrom-Json
+    $allBranchSettings = Get-Content -Path (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) $SettingsFilePath) | Out-String | ConvertFrom-Json
     $newBranchSettings = @()
     $allBranchSettings.Branches | Where-Object -Property branchId -NE $BranchSettings.branchId | foreach {$newBranchSettings += $_}
     Set-Content -Path (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) $SettingsFilePath) -Value ($newBranchSettings | ConvertTo-Json)        
--- Scripts/AdvaniaGIT/Update-BranchSettings.ps1
+++ Scripts/AdvaniaGIT/Update-BranchSettings.ps1
@@ -6,7 +6,7 @@
         [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
         [String]$SettingsFilePath = "Data\BranchSettings.Json"
     ) 
-    $allBranchSettings = Get-Content -Path (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) $SettingsFilePath) | ConvertFrom-Json
+    $allBranchSettings = Get-Content -Path (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) $SettingsFilePath) | Out-String | ConvertFrom-Json
     $newBranchSettings = @()
     $allBranchSettings.Branches | Where-Object -Property branchId -NE $BranchSettings.branchId | foreach {$newBranchSettings += $_}
     $newBranchSettings += $BranchSettings
