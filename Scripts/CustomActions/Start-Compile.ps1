a4124c1a8bee7a106802ef76db446742f08d7766
Gunnar Gestsson <gg@advania.is>
2017-05-29 15:47:16 +0000

    Change Compile to compile Test objects after the non-test objects

--- Scripts/CustomActions/Start-Compile.ps1
+++ Scripts/CustomActions/Start-Compile.ps1
@@ -4,8 +4,15 @@ $objectTypes = 'Table','Page','Report','Codeunit','Query','XMLport','MenuSuite'
 $jobs = @()
 foreach($objectType in $objectTypes) {
     Write-Host "Starting $objectType compilation..."
-    $jobs += Compile-NAVApplicationObject -DatabaseServer (Get-DatabaseServer -BranchSettings $BranchSettings) -DatabaseName $BranchSettings.databasename -Filter Type=$objectType -AsJob -NavServerName localhost -NavServerInstance $BranchSettings.instanceName -NavServerManagementPort $BranchSettings.managementServicesPort -LogPath $LogPath -SynchronizeSchemaChanges Yes -Recompile    
+    $filter = "Type=$objectType;Version List=<>*Test*"
+    $jobs += Compile-NAVApplicationObject -DatabaseServer (Get-DatabaseServer -BranchSettings $BranchSettings) -DatabaseName $BranchSettings.databasename -Filter $filter -AsJob -NavServerName localhost -NavServerInstance $BranchSettings.instanceName -NavServerManagementPort $BranchSettings.managementServicesPort -LogPath $LogPath -SynchronizeSchemaChanges Yes -Recompile    
 }
  
+Receive-Job -Job $jobs -Wait
+foreach($objectType in $objectTypes) {
+    Write-Host "Starting $objectType test objects compilation..."
+    $filter = "Type=$objectType;Version List=*Test*"
+    $jobs += Compile-NAVApplicationObject -DatabaseServer (Get-DatabaseServer -BranchSettings $BranchSettings) -DatabaseName $BranchSettings.databasename -Filter $filter -AsJob -NavServerName localhost -NavServerInstance $BranchSettings.instanceName -NavServerManagementPort $BranchSettings.managementServicesPort -LogPath $LogPath -SynchronizeSchemaChanges Yes -Recompile    
+}
 Receive-Job -Job $jobs -Wait
 UnLoad-IdeTools
