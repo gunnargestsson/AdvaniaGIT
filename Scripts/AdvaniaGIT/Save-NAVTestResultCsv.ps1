cb674df5e05d4eae17cdd5dcccd8a14038fb829a
Gunnar Gestsson <gg@advania.is>
2017-05-31 09:19:59 +0000

    Save results to CSV file to be opened by Excel.  A better user experience

---
+++ Scripts/AdvaniaGIT/Save-NAVTestResultCsv.ps1
@@ -0,0 +1,16 @@
+﻿function Save-NAVTestResultCsv
+{
+    param(
+        $SQLServer,
+        $SQLDb,
+        $ResultTableName,
+        $OutFile
+    )
+    $Command = "select [No_],[Test Run No_],[Codeunit ID],[Codeunit Name],[Function Name],"+`
+                "[Platform],CASE [Result] WHEN 0 THEN 'Passed' WHEN 1 THEN 'Failed' WHEN 2 THEN 'Inconclusive' ELSE 'Incomplete' END as [Result],[Restore],[Execution Time],[Error Code],[Error Message],"+`
+                "[File],[Call Stack],[User ID], CONVERT(VARCHAR(50),[Start Time], 127) as [Start Time2],"+`
+                "CONVERT(VARCHAR(50),[Finish Time], 127) as [Finish Time2] from [$ResultTableName]"
+    $SqlResult = Get-SQLCommandResult -Server $SQLServer -Database $SQLDb -Command $Command 
+    $SqlResult | Export-Csv -Path $OutFile -Encoding Default -NoTypeInformation -Delimiter ";"
+}
+    
---
+++ Scripts/CustomActions/Save-TestResultsCsv.ps1
@@ -0,0 +1,11 @@
+﻿Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+if ($SetupParameters.testCompanyName) {
+    $companyName = $SetupParameters.testCompanyName
+} else {
+    $companyName = Get-FirstCompanyName -SQLServer (Get-DatabaseServer -BranchSettings $BranchSettings) -SQLDb $BranchSettings.databaseName
+}
+$ResultTableName = Get-DatabaseTableName -CompanyName $companyName -TableName 'CAL Test Result'
+$OutFile = Join-Path $LogPath ((Split-Path $LogPath -Leaf) + ".csv")
+Save-NAVTestResultCsv -SQLServer (Get-DatabaseServer -BranchSettings $BranchSettings) -SQLDb $BranchSettings.databaseName -ResultTableName $ResultTableName -OutFile $OutFile 
+
+& $OutFile
