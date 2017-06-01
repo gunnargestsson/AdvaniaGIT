c8bbd49aa695761b6da31add82fbb0f43115ea67
Gunnar Gestsson <gg@advania.is>
2017-06-01 17:00:03 +0000

    Don't remove license from the bacpac file

--- Scripts/CustomActions/Create-NavBacpac.ps1
+++ Scripts/CustomActions/Create-NavBacpac.ps1
@@ -1,12 +1,7 @@
 ﻿Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings
 Write-Host "Requesting new NAV bacpac for branch" $SetupParameters.Branchame
-Write-Host "Removing NAV License from database before backing up..."
-Remove-NAVDatabaseLicense -BranchSettings $BranchSettings
 Load-InstanceAdminTools -SetupParameters $SetupParameters
 Set-NAVServerInstance -ServerInstance $BranchSettings.instanceName -Stop -Force
 Create-NAVDatabaseBacpac -SetupParameters $SetupParameters -BranchSettings $BranchSettings
 Set-NAVServerInstance -ServerInstance $BranchSettings.instanceName -Start -Force -ErrorAction Stop
-if (Test-Path $SetupParameters.LicenseFilePath) {  
-    Update-NAVLicense -BranchSettings $BranchSettings -LicenseFilePath $SetupParameters.LicenseFilePath 
-}
 UnLoad-InstanceAdminTools 
