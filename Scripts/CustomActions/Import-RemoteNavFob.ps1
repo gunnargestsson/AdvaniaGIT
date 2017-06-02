15d245cf0e4d3ac2bcd0bb55237b29ecaa700931
Gunnar Gestsson <gg@advania.is>
2017-06-02 17:23:07 +0000

    Import Remote NAV Update

--- Scripts/CustomActions/Import-RemoteNavFob.ps1
+++ Scripts/CustomActions/Import-RemoteNavFob.ps1
@@ -7,22 +7,22 @@ try {
     $TargetFolder = 'c:\AdvaniaGIT\Workspace'
     Add-Type -assembly 'system.io.compression.filesystem'
 
-    Remove-Item $LocalArchive -Force -ErrorAction SilentlyContinue
+    Remove-Item $LocalArchive -Force -ErrorAction SilentlyContinue| Out-Null
     Write-Verbose 'Compressing the file'
     [io.compression.zipfile]::CreateFromDirectory($env:bamboo_working_directory,$LocalArchive)
 
     $session = Invoke-RemoteCommand -Command 'import-module "C:\Program Files\Microsoft Dynamics NAV\100\Service\Microsoft.Dynamics.Nav.Management.dll"' -Verbose 
     Write-Verbose 'Copying file to remote machine'
-    Invoke-RemoteCommand -Command 'remove-item '$RemoteFile' -Force -ErrorAction SilentlyContinue' -Verbose -PSSession $session
+    Invoke-RemoteCommand -Command "remove-item '$RemoteFile' -Force -ErrorAction SilentlyContinue" -Verbose -PSSession $session | Out-Null
     Copy-FileToRemoteMachine -SourceFile $LocalArchive -DestinationFile $RemoteFile -Session $session | Out-Null
-    Remove-Item -Path $LocalArchive -Force
+    Remove-Item -Path $LocalArchive -Force | Out-Null
     Write-Verbose 'Copying finished'
-    Invoke-RemoteCommand -Command 'Add-Type -assembly "system.io.compression.filesystem"' -Verbose -PSSession $session
-    Invoke-RemoteCommand -Command "[io.compression.zipfile]::ExtractToDirectory('$RemoteFile','$TargetFolder')" -Verbose -PSSession $session
-    Invoke-RemoteCommand -Command "remove-item '$RemoteFile' -Force" -Verbose -PSSession $session
-    Invoke-RemoteCommand -Command 'import-module AdvaniaGIT' -Verbose -PSSession $session
-    Invoke-RemoteCommand -Command ". C:\AdvaniaGIT\Scripts\CustomActions\Import-NavFob.ps1 -DatabaseName '$($env:bamboo_AzureDB_DatabaseName)' -DatabaseServer '$($env:bamboo_AzureDB_ServerName)' -Path (Get-ChildItem -Path $RemoteFolder -Filter *.fob).FullName -Verbose" -Verbose -PSSession $session
-    Invoke-RemoteCommand -Command 'remove-item -Path (Get-ChildItem -Path $RemoteFolder -Filter *.fob).FullName -Force' -Verbose -CloseSession -PSSession $session
+    Invoke-RemoteCommand -Command 'Add-Type -assembly "system.io.compression.filesystem"' -Verbose -PSSession $session | Out-Null
+    Invoke-RemoteCommand -Command "[io.compression.zipfile]::ExtractToDirectory('$RemoteFile','$TargetFolder')" -Verbose -PSSession $session| Out-Null
+    Invoke-RemoteCommand -Command "remove-item '$RemoteFile' -Force" -Verbose -PSSession $session| Out-Null
+    Invoke-RemoteCommand -Command 'import-module AdvaniaGIT' -Verbose -PSSession $session| Out-Null
+    Invoke-RemoteCommand -Command ". C:\AdvaniaGIT\Scripts\CustomActions\Import-NavFob.ps1 -DatabaseName '$($env:bamboo_AzureDB_DatabaseName)' -DatabaseServer '$($env:bamboo_AzureDB_ServerName)' -Path (Get-ChildItem -Path $RemoteFolder -Filter *.fob).FullName -Verbose" -Verbose -PSSession $session| Out-Null
+    Invoke-RemoteCommand -Command 'remove-item -Path (Get-ChildItem -Path $RemoteFolder -Filter *.fob).FullName -Force' -Verbose -CloseSession -PSSession $session| Out-Null
 } catch {
     throw $_
 }
