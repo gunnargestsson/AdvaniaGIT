45983d176db37421afd5c393ce1dae8b02bc4e66
Gunnar Gestsson <gunnar@dynamics.is>
2017-06-12 15:38:44 +0000

    Updates to the version filtering when working on service instances

--- Scripts/CustomActions/Prepare-NAVEnvironment.ps1
+++ Scripts/CustomActions/Prepare-NAVEnvironment.ps1
@@ -2,17 +2,17 @@
 #Import-Module (Join-Path $setupParameters.navServicePath 'Microsoft.Dynamics.Nav.Management.dll') -Scope Local
 
 #Stop NAV Server Instances
-Get-NAVServerInstance | Where-Object -Property Version -Match ($SetupParameters.navVersion.Substring(0,2) + ".*.0") | Set-NAVServerInstance -Stop
+Get-NAVServerInstance | Where-Object -Property Version -Like ($SetupParameters.navVersion.Substring(0,2) + "*.0") | Set-NAVServerInstance -Stop
         
 #Update Startup Type and Dependency on NAV Server Instances
-Get-NAVServerInstance | Where-Object -Property Version -Match ($SetupParameters.navVersion.Substring(0,2) + ".*.0") | foreach {
+Get-NAVServerInstance | Where-Object -Property Version -Like ($SetupParameters.navVersion.Substring(0,2) + "*.0") | foreach {
     $branchSetting = @{instanceName = $($_.ServerInstance)}
     Enable-TcpPortSharingForNAVService -branchSetting $branchSetting
     Enable-DelayedStartForNAVService -branchSetting $branchSetting
 }
 #Start NAV Server Instances
 Write-Host "Starting Services for version $($SetupParameters.navVersion.Substring(0,2))"
-Get-NAVServerInstance | Where-Object -Property Version -Match ($SetupParameters.navVersion.Substring(0,2) + ".*.0") | Set-NAVServerInstance -Start
+Get-NAVServerInstance | Where-Object -Property Version -Like ($SetupParameters.navVersion.Substring(0,2) + "*.0") | Set-NAVServerInstance -Start
 
 UnLoad-InstanceAdminTools
 
--- Scripts/CustomActions/Stop-NAVServices.ps1
+++ Scripts/CustomActions/Stop-NAVServices.ps1
@@ -1,4 +1,4 @@
 ﻿Load-InstanceAdminTools -setupParameters $setupParameters 
 Write-Host "Stopping Services for version $($SetupParameters.navVersion.Substring(0,2))"
-Get-NAVServerInstance | Where-Object -Property Version -Match ($SetupParameters.navVersion.Substring(0,2) + ".*.0") | Set-NAVServerInstance -Stop
+Get-NAVServerInstance | Where-Object -Property Version -Like ($SetupParameters.navVersion.Substring(0,2) + "*.0") | Set-NAVServerInstance -Stop
 UnLoad-InstanceAdminTools
