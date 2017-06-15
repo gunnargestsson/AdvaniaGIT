2d553a60ba52a0b8a72d3a1b463fd4780297d66e
Gunnar Gestsson <gunnar@dynamics.is>
2017-06-15 15:55:12 +0000

    Tested and fixed for 2017 to...

--- Scripts/AdvaniaGIT/Update-CurrentInstallSource.ps1
+++ Scripts/AdvaniaGIT/Update-CurrentInstallSource.ps1
@@ -18,20 +18,13 @@
             $_.PSObject.Properties |
             foreach {                
                 if ($_.Value -like "$($CurrentInstallSource)*") {
-                    Write-Verbose -Message "Path:`t`t"$CurrentUserShellFoldersPath
-                    Write-Verbose -Message "SID:`t`t"$SID
-                    Write-Verbose -Message "Name:`t`t"$_.Name
-                    Write-Verbose -Message "Old Value:`t"$_.Value
                     $newValue = ($_.Value).Replace($CurrentInstallSource,$NewInstallSource)
-                    Write-Verbose -Message "New Value:`t"$newValue
                     Set-ItemProperty -Path $CurrentUserShellFoldersPath -Name $_.Name -Value $newValue
-                    Write-Verbose -Message "================================================================"
                 }
             }
         }
     }
     Remove-PSDrive -Name HKCR -Force -ErrorAction SilentlyContinue
-    Update-RegistryStringValue -RegistryPath "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\DynamicsNav$MainVersion" -Name "InstallSource" -Value $installWorkFolder
-    Update-RegistryStringValue -RegistryPath "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\DynamicsNav$MainVersion" -Name "SourcePath" -Value $installWorkFolder
-
+    Update-RegistryStringValue -RegistryPath "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\DynamicsNav$MainVersion" -Name "InstallSource" -Value $NewInstallSource
+    Update-RegistryStringValue -RegistryPath "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\DynamicsNav$MainVersion" -Name "SourcePath" -Value $NewInstallSource
 }
