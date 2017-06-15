9e390c1170f911082d685117f8d3245acd59b292
Gunnar Gestsson <gunnar@dynamics.is>
2017-06-15 14:31:17 +0000

    Bugfixes for Registry Update

--- Scripts/AdvaniaGIT/Update-RegistryStringValue.ps1
+++ Scripts/AdvaniaGIT/Update-RegistryStringValue.ps1
@@ -11,8 +11,8 @@
 
     if(!(Test-Path $registryPath)) {
         New-Item -Path $registryPath -Force | Out-Null
-        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null
     } else {
-        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+        New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType String -Force | Out-Null
     }
 }
