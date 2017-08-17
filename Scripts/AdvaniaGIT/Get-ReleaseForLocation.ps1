3059eccf8891d57de7f508b3061d96f3b946f6f1
Gunnar Gestsson <gunnar@dynamics.is>
2017-08-17 14:10:54 +0000

    Link CulturInfo with NAV releases

---
+++ Scripts/AdvaniaGIT/Get-ReleaseForLocation.ps1
@@ -0,0 +1,17 @@
+﻿Function Get-ReleaseForLocation
+{
+    Param(
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [String]$Location
+    )
+    PROCESS
+    {
+        Write-Host "Downloading CU information from Microsoft Blog..."
+        Switch ($Location) {
+            'US' { return 'NA' }
+            'CA' { return 'NA' }
+            'GB' { return 'UK' }
+            Default { return $Location }
+        }
+    }
+}
