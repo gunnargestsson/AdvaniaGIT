f3e10c8652c67f7d7470c7bdeaf94a355001c3e6
Gunnar Gestsson <gunnar@dynamics.is>
2017-08-23 21:22:17 +0000

    Bugfix

--- Scripts/AdvaniaGIT/Update-BranchSettings.ps1
+++ Scripts/AdvaniaGIT/Update-BranchSettings.ps1
@@ -11,5 +11,5 @@
     $allBranchSettings.Branches | Where-Object -Property branchId -NE $BranchSettings.branchId | foreach {$newBranchSettings += $_}
     $newBranchSettings += $BranchSettings
     $allBranchSettings.Branches = $newBranchSettings
-    Set-Content -Path (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) $SettingsFilePath) -Value ($allBranchSettings | ConvertTo-Json)             
+    Set-Content -Path $SettingsFilePath -Value ($allBranchSettings | ConvertTo-Json)             
 }
