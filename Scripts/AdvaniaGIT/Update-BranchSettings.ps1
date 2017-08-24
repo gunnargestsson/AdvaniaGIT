795c23dd92916f13d7ae68b262cb608cfd4ed367
Gunnar Gestsson <gunnar@dynamics.is>
2017-08-24 10:47:25 +0000

    Remove unwanted space

--- Scripts/AdvaniaGIT/Update-BranchSettings.ps1
+++ Scripts/AdvaniaGIT/Update-BranchSettings.ps1
@@ -6,7 +6,7 @@
         [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
         [String]$SettingsFilePath = (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) "Data\BranchSettings.Json")
     ) 
-    $allBranchSettings = Get-Content -Path  $SettingsFilePath | Out-String | ConvertFrom-Json
+    $allBranchSettings = Get-Content -Path $SettingsFilePath | Out-String | ConvertFrom-Json
     $newBranchSettings = @()
     $allBranchSettings.Branches | Where-Object -Property branchId -NE $BranchSettings.branchId | foreach {$newBranchSettings += $_}
     $newBranchSettings += $BranchSettings
