9f8d44587945983c9203f38bd849c571b931b9e2
Gunnar Gestsson <gunnar@dynamics.is>
2017-08-31 15:26:47 +0000

    Use navVersion property to populate app.json

--- Scripts/AdvaniaGIT/New-NAVAppJson.ps1
+++ Scripts/AdvaniaGIT/New-NAVAppJson.ps1
@@ -19,13 +19,13 @@
     $AppSettings | Add-Member -MemberType NoteProperty -Name publisher -Value $SetupParameters.appPublisher
     $AppSettings | Add-Member -MemberType NoteProperty -Name brief -Value $SetupParameters.appBriefDescription
     $AppSettings | Add-Member -MemberType NoteProperty -Name description -Value $SetupParameters.appManifestDescription
-    $AppSettings | Add-Member -MemberType NoteProperty -Name version -Value $SetupParameters.appVersion
+    $AppSettings | Add-Member -MemberType NoteProperty -Name version -Value "$($SetupParameters.navVersion.Split(".")[0]).0.0.0"
     $AppSettings | Add-Member -MemberType NoteProperty -Name privacyStatement -Value $SetupParameters.appPrivacyStatement
     $AppSettings | Add-Member -MemberType NoteProperty -Name EULA -Value $SetupParameters.appEula
     $AppSettings | Add-Member -MemberType NoteProperty -Name help -Value $SetupParameters.appHelp
     $AppSettings | Add-Member -MemberType NoteProperty -Name url -Value $SetupParameters.appUrl
     $AppSettings | Add-Member -MemberType NoteProperty -Name logo -Value $SetupParameters.appIcon
-    $AppSettings | Add-Member -MemberType NoteProperty -Name platform -Value $SetupParameters.navVersion
+    $AppSettings | Add-Member -MemberType NoteProperty -Name platform -Value "$($SetupParameters.navVersion.Split(".")[0]).0.0.0"
         
     foreach ($Property in @(Get-Member -InputObject $AppSettings -MemberType NoteProperty).Name) {
         if ($AppSettings."$Property" -eq $null) { $AppSettings."$Property" = "" }
@@ -42,7 +42,7 @@
 
     # Add Application Settings Objects
     $ApplicationSettings = New-Object -TypeName PSObject
-    $ApplicationSettings | Add-Member -MemberType NoteProperty -Name version -Value $SetupParameters.navVersion
+    $ApplicationSettings | Add-Member -MemberType NoteProperty -Name version -Value "$($SetupParameters.navVersion.Split(".")[0]).0.0.0"
     $ApplicationSettings | Add-Member -MemberType NoteProperty -Name locale -Value $SetupParameters.navSolution
     $AppSettings | Add-Member -MemberType NoteProperty -Name application -Value $ApplicationSettings
 
