dbb4c5bbf2af722386c349a8c4085a2afb518f2e
Gunnar Gestsson <gunnar@dynamics.is>
2017-09-05 16:52:22 +0000

    Script to copy License files from network drive to ftp server

---
+++ Scripts/CopyLicensesToFtpServer.ps1
@@ -0,0 +1,28 @@
+﻿Get-Module -Name AdvaniaGIT | Remove-Module
+Import-Module -Name AdvaniaGIT -DisableNameChecking
+
+$SetupParameters = Get-GITSettings
+$Licenses = Get-ChildItem -Path "V:\Allir_Advania\VL\Umsjónartól - leyfi" -Recurse -Filter "*.flf"
+$i = 0
+$count = $Licenses.Count
+$StartTime = Get-Date
+foreach ($License in $Licenses | Sort-Object -Property LastWriteTime) {
+    $i++
+    $NowTime = Get-Date
+    $TimeSpan = New-TimeSpan $StartTime $NowTime
+    $percent = $i / $count
+    if ($percent -gt 1) 
+    {
+        $percent = 1
+    }
+    $remtime = $TimeSpan.TotalSeconds / $percent * (1-$percent)
+    if (($i % 10) -eq 0) 
+    {
+        Write-Progress -Status "Processing $i of $count" -Activity 'Copying license...' -PercentComplete ($percent*100) -SecondsRemaining $remtime
+    }
+    $LicenseName = $License.BaseName.SubString(0,7)
+    if ($LicenseName -match "^[\d\.]+$") {
+        $FtpFileName = "License/${LicenseName}.flf"
+        Put-FtpFile -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -FtpFilePath $FtpFileName -LocalFilePath $License.FullName
+    }        
+}
