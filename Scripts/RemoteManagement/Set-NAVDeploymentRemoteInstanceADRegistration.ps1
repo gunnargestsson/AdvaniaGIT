130b211e4bcdbcb596c86ab9e300ca6832ac6a65
Gunnar Gestsson <gunnar@dynamics.is>
2017-09-11 10:33:46 +0000

    Limit to the no. of Access policies in key vault solved by always use the client id of the Default application

--- Scripts/RemoteManagement/Set-NAVDeploymentRemoteInstanceADRegistration.ps1
+++ Scripts/RemoteManagement/Set-NAVDeploymentRemoteInstanceADRegistration.ps1
@@ -22,17 +22,32 @@
                 Write-Host "Updating $($RemoteComputer.HostName)..."
                 $Session = New-NAVRemoteSession -Credential $Credential -HostName $RemoteComputer.FQDN
                 $ServerInstances = Get-NAVRemoteInstances -Session $Session 
-                foreach ($ServerInstance in $ServerInstances) {                    
+                $DefaultServerInstance = $ServerInstances | Where-Object -Property Default -eq True
+
+                $CertValue = Get-NAVServiceCertificateValue -Session $Session -ServerInstance $DefaultServerInstance 
+                $KeyVaultKey = Get-NAVAzureKeyVaultKey -KeyVault $KeyVault -ServerInstanceName $DefaultServerInstance.ServerInstance
+                $DefaultApplication = Get-NAVADApplication -DeploymentName $DeploymentName -ServerInstance $DefaultServerInstance -IconFilePath $IconFilePath -CertValue $CertValue
+                $ServicePrincipal = Get-NAVADServicePrincipal -ADApplication $DefaultApplication                   
+                Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ServicePrincipalName $ServicePrincipal.ServicePrincipalNames[1] -PermissionsToKeys encrypt,decrypt,get
+                Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ApplicationId $DefaultApplication.ApplicationId -ObjectId $DefaultApplication.ObjectId -PermissionsToKeys encrypt,decrypt,get
+                $DefaultServerInstance = Combine-Settings $DefaultServerInstance $KeyVault -Prefix KeyVault
+                $DefaultServerInstance = Combine-Settings $DefaultServerInstance $KeyVaultKey -Prefix KeyVaultKey
+                $DefaultServerInstance = Combine-Settings $DefaultServerInstance $ServicePrincipal -Prefix ServicePrincipal
+                $DefaultServerInstance = Combine-Settings $DefaultServerInstance $DefaultApplication -Prefix ADApplication
+                $DefaultServerInstance | Add-Member -MemberType NoteProperty -Name ADApplicationFederationMetadataLocation -Value "https://login.windows.net/$($Subscription.Account.Id.Split("@").GetValue(1))/federationmetadata/2007-06/federationmetadata.xml"
+                Set-NAVRemoteInstanceADRegistration -Session $Session -ServerInstance $DefaultServerInstance -RestartServerInstance
+
+                foreach ($ServerInstance in $ServerInstances | Where-Object -Property Default -eq False) {                    
                     $CertValue = Get-NAVServiceCertificateValue -Session $Session -ServerInstance $ServerInstance 
                     $KeyVaultKey = Get-NAVAzureKeyVaultKey -KeyVault $KeyVault -ServerInstanceName $ServerInstance.ServerInstance
                     $Application = Get-NAVADApplication -DeploymentName $DeploymentName -ServerInstance $ServerInstance -IconFilePath $IconFilePath -CertValue $CertValue
                     $ServicePrincipal = Get-NAVADServicePrincipal -ADApplication $Application                   
-                    Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ServicePrincipalName $ServicePrincipal.ServicePrincipalNames[1] -PermissionsToKeys encrypt,decrypt,get
-                    Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ApplicationId $Application.ApplicationId -ObjectId $Application.ObjectId -PermissionsToKeys encrypt,decrypt,get
+                    Remove-AzureRmKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ServicePrincipalName $ServicePrincipal.ServicePrincipalNames[1] -ErrorAction SilentlyContinue
+                    Remove-AzureRmKeyVaultAccessPolicy -VaultName $KeyVault.VaultName -ApplicationId $Application.ApplicationId -ObjectId $Application.ObjectId -ErrorAction SilentlyContinue
                     $ServerInstance = Combine-Settings $ServerInstance $KeyVault -Prefix KeyVault
                     $ServerInstance = Combine-Settings $ServerInstance $KeyVaultKey -Prefix KeyVaultKey
                     $ServerInstance = Combine-Settings $ServerInstance $ServicePrincipal -Prefix ServicePrincipal
-                    $ServerInstance = Combine-Settings $ServerInstance $Application -Prefix ADApplication
+                    $ServerInstance = Combine-Settings $ServerInstance $DefaultApplication -Prefix ADApplication
                     $ServerInstance | Add-Member -MemberType NoteProperty -Name ADApplicationFederationMetadataLocation -Value "https://login.windows.net/$($Subscription.Account.Id.Split("@").GetValue(1))/federationmetadata/2007-06/federationmetadata.xml"
                     Set-NAVRemoteInstanceADRegistration -Session $Session -ServerInstance $ServerInstance -RestartServerInstance
                 }
