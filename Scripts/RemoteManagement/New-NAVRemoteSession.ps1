058179ddb20b1abb65ac248d94f661aa55cf1f09
Gunnar Gestsson <gunnar@dynamics.is>
2017-11-10 11:31:41 +0000

    Remove the need for a version in AdvaniaGIT\Data\GITSettings.json.  Version is now from the Service version info.

--- Scripts/RemoteManagement/New-NAVRemoteSession.ps1
+++ Scripts/RemoteManagement/New-NAVRemoteSession.ps1
@@ -39,7 +39,7 @@
             if (Test-Path "$($Env:ProgramFiles)\Microsoft Dynamics NAV\*\Service") {
                 $SetupParameters | Add-Member "navServicePath" (Get-Item -Path "$($Env:ProgramFiles)\Microsoft Dynamics NAV\*\Service").FullName
                 $SetupParameters | Add-Member "mainVersion" (Split-Path (Split-Path $SetupParameters.navServicePath -Parent) -Leaf)
-                $SetupParameters | Add-Member "navRelease" (Get-NAVRelease -mainVersion $mainVersion)
+                $SetupParameters | Add-Member "navRelease" (Get-NAVRelease -mainVersion (Split-Path (Split-Path $SetupParameters.navServicePath -Parent) -Leaf))
                 $SetupParameters | Add-Member "navVersion" (Get-Item -Path (Join-Path $SetupParameters.navServicePath "Microsoft.Dynamics.Nav.Server.exe")).VersionInfo.ProductVersion
             }
             if (Test-Path "$(${env:ProgramFiles(x86)})\Microsoft Dynamics NAV\*\Roletailored Client") {
