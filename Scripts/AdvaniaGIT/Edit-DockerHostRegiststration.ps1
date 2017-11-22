b658bf737b98062dbdc25edaf9b5b50eb2b175ae
Gunnar Gestsson <gunnar@dynamics.is>
2017-11-22 15:59:47 +0000

    Add Try function to host file edit

--- Scripts/AdvaniaGIT/Edit-DockerHostRegiststration.ps1
+++ Scripts/AdvaniaGIT/Edit-DockerHostRegiststration.ps1
@@ -3,32 +3,47 @@
     param(
         [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
         [String]$RemoveHostName,
-        [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyname=$true)]
         [String]$AddHostName,
         [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
         [String]$AddIpAddress
     )
 
     $hostsPath = Get-Item -Path (Join-Path $env:SystemRoot "System32\Drivers\etc\hosts")
-    $newHostsContent = @()
-    if ($hostsPath.Length -eq 0) {
-        $newHostsContent += "127.0.0.1    localhost"
-    }     
-    if ($hostsPath.Length -gt 0) {
-        $hostsContent = Get-Content -Path $hostsPath -Encoding Ascii
-        foreach ($hostsLine in $hostsContent) {
-            if ($RemoveHostName -ne $null -and $RemoveHostName -gt "" -and $hostsLine -imatch $RemoveHostName) {
-              Write-Verbose "Removing ${hostsLine}..."
-            } elseif ($hostsLine -gt "") {
-                $newHostsContent += $hostsLine 
-            }
+    $hostsContent = $null
+    while (!($HostsContent)) {
+        try {
+            $hostsContent = Get-Content -Path $hostsPath -Encoding Ascii
+        } 
+        catch {
+            Start-Sleep -Seconds 1
         }
     }
+    
+    $newHostsContent = @()
+    foreach ($hostsLine in $hostsContent) {
+        if ($RemoveHostName -ne $null -and $RemoveHostName -gt "" -and $hostsLine -imatch $RemoveHostName) {
+            Write-Verbose "Removing ${hostsLine}..."
+        } elseif ($hostsLine -gt "") {
+            $newHostsContent += $hostsLine 
+        }
+    }
+
+    
     if ($AddHostName -ne $null -and $AddIpAddress -ne $null) {
+        Write-Verbose "Adding ${AddHostName}..."
         $newHostsContent += "${AddIpAddress}    ${AddHostName}" + "`r`n"
     }
-    if ($newHostsContent.Length -gt 0) {
-        Remove-Item -Path $hostsPath -Force -ErrorAction SilentlyContinue
-        Set-Content -Encoding Ascii -Path $hostsPath -Value $newHostsContent 
+
+    $hostsWritten = $null
+    while (!($hostsWritten)) {
+        try {
+            Remove-Item -Path $hostsPath -Force -ErrorAction SilentlyContinue
+            Set-Content -Encoding Ascii -Path $hostsPath -Value $newHostsContent 
+            $hostsWritten = $true
+        }
+        catch {
+            Start-Sleep -Seconds 1
+        }
     }
 }
