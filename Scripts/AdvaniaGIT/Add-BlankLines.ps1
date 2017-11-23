76750e90cba27b84111f1f64cdfc4027b4ef12a8
Gunnar Gestsson <gunnar@dynamics.is>
2017-11-23 11:40:31 +0000

    Don't add lines if running inside a containers

--- Scripts/AdvaniaGIT/Add-BlankLines.ps1
+++ Scripts/AdvaniaGIT/Add-BlankLines.ps1
@@ -6,7 +6,7 @@
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyname=$true)]
         [int]$NoOfLines = 10
     )
-    if ($env:TERM_PROGRAM -eq $null) {
+    if ($env:TERM_PROGRAM -eq $null -and $env:username -ne "ContainerAdministrator") {
         For ($i=0; $i -le $NoOfLines; $i++) { Write-Host "" }
     }
 }
