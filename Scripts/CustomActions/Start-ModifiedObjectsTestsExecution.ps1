98d8bc1fd368aeee6c21180d0852824615c7f136
Gunnar Gestsson <gunnar@dynamics.is>
2017-11-25 18:24:34 +0000

    Add Custom Actions to execute tests from VS Code

---
+++ Scripts/CustomActions/Start-ModifiedObjectsTestsExecution.ps1
@@ -0,0 +1,12 @@
+﻿Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+if ($SetupParameters.testCompanyName) {
+    $companyName = $SetupParameters.testCompanyName
+} else {
+    $companyName = Get-FirstCompanyName -SQLServer (Get-DatabaseServer -BranchSettings $BranchSettings) -SQLDb $BranchSettings.databaseName
+}
+
+$CompanyRegistrationNo = Initialize-NAVTestCompanyRegistrationNo -BranchSettings $BranchSettings -CompanyName $companyName
+Prepare-NAVTestExecution -BranchSettings $BranchSettings -CompanyName $companyName -ForModifiedObjects
+& (Join-Path $PSScriptRoot Prepare-NAVUnitTest.ps1) 
+& (Join-path $PSScriptRoot Start-TestClient.ps1)
+Set-NAVCompanyInfoRegistrationNo -BranchSettings $BranchSettings -CompanyName $companyName -RegistrationNo $CompanyRegistrationNo
