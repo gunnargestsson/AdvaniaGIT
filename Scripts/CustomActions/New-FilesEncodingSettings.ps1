3a4ed1de93da5c0ad8200897d89a1e822a109157
Gunnar Gestsson <gunnar@dynamics.is>
2017-11-25 18:23:39 +0000

    Add function to create a settings.json file in .vscode to support NAV object files in oem codepage

---
+++ Scripts/CustomActions/New-FilesEncodingSettings.ps1
@@ -0,0 +1,15 @@
+﻿$vsCodeSettingsFolder = Join-Path $SetupParameters.Repository ".vscode"
+New-Item -Path $vsCodeSettingsFolder -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+$vsCodeSettingsFile = Join-Path $vsCodeSettingsFolder "settings.json"
+if (Test-Path $vsCodeSettingsFile) {
+    $vsCodeSettings = Get-Content -Path $vsCodeSettingsFile -Encoding UTF8 | Out-String | ConvertFrom-Json
+} else {
+    $vsCodeSettings = New-Object -TypeName PSObject
+}
+
+if (![bool]($vsCodeSettings.PSObject.Properties.name -match "files.encoding")) {
+    $vsCodeSettings | Add-Member -MemberType NoteProperty -Name "files.encoding" -Value ""
+}
+
+$vsCodeSettings.'files.encoding' = $SetupParameters.filesEncoding
+Set-Content -PassThru $vsCodeSettingsFile -Value (ConvertTo-Json -InputObject $vsCodeSettings) -Encoding UTF8 -Force | Out-Null
