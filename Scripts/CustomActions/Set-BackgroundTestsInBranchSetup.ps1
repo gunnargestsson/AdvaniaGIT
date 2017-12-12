583e873032ffc55a7015b764b79709e0966385f6
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-12 10:28:28 +0000

    Custom action to switch tests to background mode for build process

---
+++ Scripts/CustomActions/Set-BackgroundTestsInBranchSetup.ps1
@@ -0,0 +1,3 @@
+﻿$SetupJson = Get-Content $SetupParameters.setupPath -Encoding UTF8 | Out-String | ConvertFrom-Json
+$SetupJson | Add-Member -MemberType NoteProperty -Name testExecution -Value "Background" -Force
+Set-Content -Value ($SetupJson | ConvertTo-Json) -Encoding UTF8 -Path $SetupParameters.setupPath
