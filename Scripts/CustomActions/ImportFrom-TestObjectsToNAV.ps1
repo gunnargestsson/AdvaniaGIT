0a7d7721d3663defb23520cf8fcb9efdc3b9f063
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-12 09:21:54 +0000

    New custom actions for the build process

---
+++ Scripts/CustomActions/ImportFrom-TestObjectsToNAV.ps1
@@ -0,0 +1,18 @@
+﻿Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings 
+if ($BranchSettings.dockerContainerId -gt "") {
+    Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name
+} else {    
+    if (Test-Path $SetupParameters.testObjectsPath) {
+        foreach ($testObjectFile in (Get-ChildItem -Path $SetupParameters.testObjectsPath)) {
+            Update-NAVApplicationFromTxt -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $testObjectFile.FullName -SkipDeleteCheck
+        }
+    }
+    if (Test-Path (Join-Path $env:SystemDrive 'TestToolKit')) {
+        foreach ($testObjectFile in (Get-ChildItem -Path (Join-Path $env:SystemDrive 'TestToolKit'))) {
+            Update-NAVApplicationFromTxt -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $testObjectFile.FullName -SkipDeleteCheck
+        }
+    }
+
+}
+        
+
---
+++ Scripts/CustomActions/Set-DockerInBranchSetup.ps1
@@ -0,0 +1,9 @@
+﻿$SetupJson = Get-Content $SetupParameters.setupPath -Encoding UTF8 | Out-String | ConvertFrom-Json
+if ($($SetupJson.navSolution.SubString(0,2)) -eq "W1") {
+    $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "microsoft/dynamics-nav:$($SetupJson.navBuild)" -Force
+} else {
+    $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "microsoft/dynamics-nav:$($SetupJson.navBuild)-$($SetupJson.navSolution.SubString(0,2).ToLower())" -Force
+}
+
+
+Set-Content -Value ($SetupJson | ConvertTo-Json) -Encoding UTF8 -Path $SetupParameters.setupPath
