9b0c1c8d9da86b747a3cefbde890089d89fccd69
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-14 15:24:28 +0000

    New custom action to download client addins

---
+++ Scripts/CustomActions/Download-ClientAddins.ps1
@@ -0,0 +1,9 @@
+﻿if ($BranchSettings.dockerContainerId -gt "") {
+    Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name
+} else {    
+    $FileName = Join-Path $SetupParameters.LogPath 'AddIns.zip'
+    Download-NAVFile -Url "https://addins.navleiga.is/addins.zip" -FileName $FileName
+    $AddInsPath = Join-Path $SetupParameters.navIdePath "Add-ins\AdvaniaGIT"
+    New-Item -Path $AddInsPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+    Expand-Archive -LiteralPath $FileName -DestinationPath $AddInsPath -Force
+}
