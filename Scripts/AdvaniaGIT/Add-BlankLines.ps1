d8a07d744df0f9835ca8ca8e850dd847cfda363b
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-14 14:38:16 +0000

    Skip 10 blank lines on bamboo builds

--- Scripts/AdvaniaGIT/Add-BlankLines.ps1
+++ Scripts/AdvaniaGIT/Add-BlankLines.ps1
@@ -6,7 +6,7 @@
         [Parameter(Mandatory=$false, ValueFromPipelineByPropertyname=$true)]
         [int]$NoOfLines = 10
     )
-    if ($env:TERM_PROGRAM -eq $null -and $env:username -ne "ContainerAdministrator") {
+    if ($env:TERM_PROGRAM -eq $null -and $env:username -ne "ContainerAdministrator" -and $env:BAMBOO_AGENT_HOME -eq $null) {
         For ($i=0; $i -le $NoOfLines; $i++) { Write-Host "" }
     }
 }
