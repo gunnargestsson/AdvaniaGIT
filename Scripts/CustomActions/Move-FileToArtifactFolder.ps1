01f150d0be603ce645c413d8f8f6be7fe44cca24
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-14 17:33:01 +0000

    Create Artifact folder if it does not exist

--- Scripts/CustomActions/Move-FileToArtifactFolder.ps1
+++ Scripts/CustomActions/Move-FileToArtifactFolder.ps1
@@ -2,6 +2,7 @@
     $SourceFile = Get-Item -Path $SetupParameters.SourceFilePath 
     if ($SourceFile) {
         Write-Host "Moving ${SourceFile} to $($SetupParameters.DestinationFilePath)..."
+        New-Item -Path $SetupParameters.DestinationFilePath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
         Move-Item -Path $SourceFile.FullName -Destination $SetupParameters.DestinationFilePath -Force -ErrorAction SilentlyContinue
     }
 }
