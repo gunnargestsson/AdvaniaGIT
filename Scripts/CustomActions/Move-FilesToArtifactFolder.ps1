b78958949b9f221a87981f17dac8db25d66c10bf
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-15 15:20:47 +0000

    Action to move all artifacts to build folder

---
+++ Scripts/CustomActions/Move-FilesToArtifactFolder.ps1
@@ -0,0 +1,17 @@
+﻿if (![String]::IsNullOrEmpty($SetupParameters.DestinationFilePath)) {
+    New-Item -Path $SetupParameters.DestinationFilePath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+
+    # Backup Folder
+    $backups = Get-ChildItem -Path $SetupParameters.backupPath -Filter "$($SetupParameters.navRelease)-$($SetupParameters.projectName).*"
+    foreach ($file in $backups) {
+        Write-Host "Moving $($file.Name) to $($SetupParameters.DestinationFilePath)..."       
+        Move-Item -Path $file.FullName -Destination $SetupParameters.DestinationFilePath -Force -ErrorAction SilentlyContinue
+    }
+
+    # Workspace Folder
+    $results = Get-ChildItem -Path $SetupParameters.WorkFolder -Filter "$($SetupParameters.navRelease)-$($SetupParameters.projectName).*"
+    foreach ($file in $resuls) {
+        Write-Host "Moving $($file.Name) to $($SetupParameters.DestinationFilePath)..."       
+        Move-Item -Path $file.FullName -Destination $SetupParameters.DestinationFilePath -Force -ErrorAction SilentlyContinue
+    }
+}
