0ecf6c9477693c6e11cbdc20e96084645fd306e9
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-15 13:42:52 +0000

    Don't copy the Visual Studio Code Extension while building

--- Scripts/CustomActions/Put-NAVBackupOnFtpSite.ps1
+++ Scripts/CustomActions/Put-NAVBackupOnFtpSite.ps1
@@ -1,9 +1,12 @@
-﻿$BackupDestinationFilePath = Join-Path $SetupParameters.BackupPath "$($SetupParameters.navRelease)-$($SetupParameters.navSolution).bak"
+﻿$BackupDestinationFilePath = Join-Path $SetupParameters.BackupPath "$($SetupParameters.navRelease)-$($SetupParameters.projectName).bak"
 if (Test-Path $BackupDestinationFilePath) {
+    Write-Host "Found file $BackupDestinationFilePath"
     if ($SetupParameters.ftpServer -gt "") {
-        Write-Host "Upload Results to $($SetupParameters.ftpServer)..."
+        Write-Host "Upload $(Split-Path $BackupDestinationFilePath -Leaf) to $($SetupParameters.ftpServer)..."
         Create-FtpDirectory -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -FtpDirectoryPath $SetupParameters.navRelease    
-        $BackupFtpDestinationPath = Join-Path $SetupParameters.navRelease "$($SetupParameters.navSolution).bak"
+        $BackupFtpDestinationPath = Join-Path $SetupParameters.navRelease "$($SetupParameters.projectName).bak"
         Put-FtpFile -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -LocalFilePath $BackupDestinationFilePath -FtpFilePath $BackupFtpDestinationPath
     }
+} else {
+    Write-Host "Did not find file $BackupDestinationFilePath"
 }
