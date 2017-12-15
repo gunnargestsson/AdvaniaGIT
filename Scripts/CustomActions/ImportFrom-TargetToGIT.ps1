cf09b92de9c366c4b044312862811c926dd61c0e
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-15 22:56:26 +0000

    Import Target into GiT moved into Docker

--- Scripts/CustomActions/ImportFrom-TargetToGIT.ps1
+++ Scripts/CustomActions/ImportFrom-TargetToGIT.ps1
@@ -1,13 +1,17 @@
 Check-GitNotUnattached
 
-Load-ModelTools -SetupParameters $SetupParameters
-$ObjectFileName = (Join-Path $SetupParameters.workFolder 'Target.txt')
+if ($BranchSettings.dockerContainerId -gt "") {
+    Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
+} else { 
+    Load-ModelTools -SetupParameters $SetupParameters
+    $ObjectFileName = (Join-Path $SetupParameters.workFolder 'Target.txt')
 
-if (Test-Path $ObjectFileName)
-{
-    Split-NAVApplicationObjectFile -Source $ObjectFileName -Destination $SetupParameters.ObjectsPath -Force
-}
-else
-{
-    Write-Error $ObjectFileName "not found!" -ErrorAction Stop
+    if (Test-Path $ObjectFileName)
+    {
+        Split-NAVApplicationObjectFile -Source $ObjectFileName -Destination $SetupParameters.ObjectsPath -Force
+    }
+    else
+    {
+        Write-Error $ObjectFileName "not found!" -ErrorAction Stop
+    }
 }
