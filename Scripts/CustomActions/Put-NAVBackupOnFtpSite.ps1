228066b77422a765ab3c4ca578f79c06468e9584
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-15 11:05:59 +0000

    Function to upload the NAV backup file

---
+++ Scripts/CustomActions/Put-NAVBackupOnFtpSite.ps1
@@ -0,0 +1,9 @@
+﻿$BackupDestinationFilePath = Join-Path $SetupParameters.BackupPath "$($SetupParameters.navRelease)-$($SetupParameters.navSolution).bak"
+if (Test-Path $BackupDestinationFilePath) {
+    if ($SetupParameters.ftpServer -gt "") {
+        Write-Host "Upload Results to $($SetupParameters.ftpServer)..."
+        Create-FtpDirectory -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -FtpDirectoryPath $SetupParameters.navRelease    
+        $BackupFtpDestinationPath = Join-Path $SetupParameters.navRelease "$($SetupParameters.navSolution).bak"
+        Put-FtpFile -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -LocalFilePath $BackupDestinationFilePath -FtpFilePath $BackupFtpDestinationPath
+    }
+}
