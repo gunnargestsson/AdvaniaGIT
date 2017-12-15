53a930925af567928d3cdd4a79c2dd11e627d352
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-15 16:59:57 +0000

    Solution build on Bamboo

---
+++ Scripts/CustomActions/Merge-NAVSolutionToTarget.ps1
@@ -0,0 +1,54 @@
+﻿if ($BranchSettings.dockerContainerId -gt "") {
+    Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
+} else {    
+    $MergeFolder = (Join-Path $SetupParameters.workFolder 'Merge')
+    $SourceFolder = (Join-Path $MergeFolder 'Base')
+    $TargetFolder = (Join-Path $MergeFolder 'TargetObjects')
+    $ConflictFolder = (Join-Path $MergeFolder 'ConflictObjects')
+
+    Remove-Item -Path $TargetFolder -Recurse -Force -ErrorAction SilentlyContinue
+    New-Item $TargetFolder -ItemType Directory | Out-Null
+
+    Remove-Item -Path $ConflictFolder -Recurse -Force -ErrorAction SilentlyContinue
+    New-Item $ConflictFolder -ItemType Directory | Out-Null
+
+    Write-Host Building Target...
+    $Deltas = Get-ChildItem -Path (Join-Path $MergeFolder 'Deltas') -Recurse
+    foreach ($Delta in $Deltas) 
+    {        
+        $BaseName = $Delta.BaseName
+        $ObjectName = Join-Path $SourceFolder ($BaseName + '.txt')
+        $TargetName = Join-Path $TargetFolder ($BaseName + '.txt')
+        $AppName = Split-Path (Split-Path $Delta.FullName -Parent) -Leaf                    
+            
+        if (Test-Path -Path $ObjectName)
+        {
+            Write-Verbose "Merging file $AppName $Delta into $ObjectName..."
+            $UpdatedObjects = Update-NAVApplicationObject -Target $ObjectName -Delta $Delta.FullName -Result $TargetName -DateTimeProperty FromModified -ModifiedProperty FromModified -VersionListProperty FromModified -DocumentationConflict ModifiedFirst -Force
+            foreach ($obj in $UpdatedObjects) 
+            { 
+                $UpdateResult = $obj.UpdateResult
+            }
+            if ($UpdateResult -eq 'Conflict')
+            {                        
+                Copy-Item -Path $Delta.FullName -Destination (Join-Path $ConflictFolder ($AppName + '_' + $Delta.Name)) -Force
+                $UpdatedObjects.Summary | Out-File -FilePath (Join-Path $ConflictFolder ($AppName + '_' + $Delta.BaseName + '.CONFLICT'))
+                Copy-Item -Path $ObjectName -Destination (Join-Path $ConflictFolder ($AppName + '_Source_' + $Delta.BaseName + '.TXT'))
+                Copy-Item -Path $TargetName -Destination (Join-Path $ConflictFolder ($AppName + '_Target_' + $Delta.BaseName + '.TXT')) -ErrorAction SilentlyContinue
+                Write-Warning "Object $AppName $Delta result: $UpdateResult"
+            }
+            else
+            {
+                Set-NAVApplicationObjectProperty -Target $TargetName -VersionListProperty (Merge-NAVVersionListString -source (Get-NAVApplicationObjectProperty -Source $ObjectName).VersionList -target (Get-NAVApplicationObjectProperty -Source $TargetName).VersionList)
+                Copy-Item -Path $TargetName -Destination $ObjectName -Force
+            }
+        }
+        elseif (Test-Path -Path $Delta.FullName -PathType Leaf)
+        {
+            Write-Verbose "Copying file $AppName $Delta into $ObjectName..."
+            Copy-Item -Path $Delta.FullName -Destination $ObjectName -Force
+        }
+    }
+    Write-Host Saving As Target.txt
+    Join-NAVApplicationObjectFile -Source (Join-Path $SourceFolder '*.txt') -Destination (Join-Path $($SetupParameters.workFolder) Target.txt) -Force
+}
---
+++ Scripts/CustomActions/Prepare-NAVSolutionBuild.ps1
@@ -0,0 +1,55 @@
+﻿# Get variables
+$Location = (Get-Location).Path
+$GitSettings = Get-GITSettings
+$sourcebranch = git.exe rev-parse --abbrev-ref HEAD 
+
+# Create GitFolder and enter it
+$TempFolder = (Join-Path $SetupParameters.workFolder 'Temp')
+Remove-Item $TempFolder -Recurse -Force -ErrorAction SilentlyContinue
+New-Item $TempFolder -ItemType Directory -Force -ErrorAction Stop | Out-Null
+Set-Location $TempFolder
+
+# Create the Merge Folder
+$MergeFolder = (Join-Path $SetupParameters.workFolder 'Merge')
+Remove-Item -Path $MergeFolder -Recurse -Force -ErrorAction SilentlyContinue
+New-Item $MergeFolder -ItemType Directory | Out-Null
+New-Item (Join-Path $MergeFolder 'Base') -ItemType Directory | Out-Null
+New-Item (Join-Path $MergeFolder 'Deltas') -ItemType Directory | Out-Null
+
+# Clone the base branch
+Write-Host Get objects from $SetupParameters.baseBranch
+git.exe clone --single-branch --branch $($SetupParameters.baseBranch) --verbose $($SetupParameters.SourceRepository) $($SetupParameters.baseBranch) --quiet
+$BranchFolder = Join-Path $TempFolder $SetupParameters.baseBranch
+$BranchSetupParameters = Combine-Settings (Get-Content (Join-Path $BranchFolder (Split-Path $SetupParameters.setupPath -Leaf)) | Out-String | ConvertFrom-Json) $GitSettings
+Write-Host Copying files from (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.objectsPath) '*.txt') to (Join-Path $MergeFolder 'Base') 
+Copy-Item -Path (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.objectsPath) '*.txt') -Destination (Join-Path $MergeFolder 'Base') -Force
+
+# Clone the delta branches
+$DeltaFolderIndexNo = 10000
+if ($SetupParameters.deltaBranchList) {
+    $deltaBranchList = ($SetupParameters.deltaBranchList).split(",")
+    foreach ($deltaBranch in $deltaBranchList)
+    {        
+        Write-Host Get deltas from $deltaBranch
+        git.exe clone --single-branch --branch $deltaBranch --verbose $($SetupParameters.SourceRepository) $deltaBranch --quiet
+        $BranchFolder = Join-Path $TempFolder $deltaBranch
+        $BranchSetupParameters = Combine-Settings (Get-Content (Join-Path $BranchFolder (Split-Path $SetupParameters.setupPath -Leaf)) | Out-String | ConvertFrom-Json) $GitSettings
+        $branchMergeFolder = (Join-Path (Join-Path $MergeFolder 'Deltas') ($DeltaFolderIndexNo.ToString() + $deltaBranch))
+        New-Item $branchMergeFolder -ItemType Directory | Out-Null
+        Write-Host Copying files from (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.deltasPath) '*.delta') to $branchMergeFolder 
+        Copy-Item -Path (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.deltasPath) '*.delta') -Destination $branchMergeFolder -Force
+        $DeltaFolderIndexNo += 10
+    }
+}
+
+# Copy Customers Deltas
+Write-Host Get deltas from Customer
+$branchMergeFolder = (Join-Path (Join-Path $MergeFolder 'Deltas') ($DeltaFolderIndexNo.ToString() + $sourcebranch))
+New-Item $branchMergeFolder -ItemType Directory | Out-Null
+Write-Host Copying files from (Join-Path $SetupParameters.deltasPath '*.delta') to $branchMergeFolder 
+Copy-Item -Path (Join-Path $SetupParameters.deltasPath '*.delta') -Destination $branchMergeFolder -Force
+$DeltaFolderIndexNo += 10
+
+# Back to Workfolder and clean GIT folder
+Set-Location $Location
+Remove-Item $TempFolder -Recurse -Force -ErrorAction SilentlyContinue
