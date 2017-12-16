437746d18dae2a7b4ffcf80d53f1d82d681a89b7
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-16 20:04:35 +0000

    Change Import from Target to catch errors

--- Scripts/CustomActions/ImportFrom-TargettoNAV.ps1
+++ Scripts/CustomActions/ImportFrom-TargettoNAV.ps1
@@ -3,6 +3,18 @@ if ($BranchSettings.dockerContainerId -gt "") {
     Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
 } else {    
     Load-ModelTools -SetupParameters $SetupParameters
-    Import-NAVApplicationGITObject -SetupParameters $SetupParameters -BranchSettings $BranchSettings -Path (Join-Path $SetupParameters.workFolder 'Target.txt') -ImportAction Overwrite -SynchronizeSchemaChanges Force        
+    
+    Write-Verbose -Message "Importing from Target.txt on $($BranchSettings.managementPort)/$($BranchSettings.instanceName)"
+    $logFile = (Join-Path $SetupParameters.LogPath "navimport.log")
+    $command = "Command=ImportObjects`,ImportAction=Overwrite`,SynchronizeSchemaChanges=Force`,File=`"(Join-Path $SetupParameters.workFolder 'Target.txt')`""                 
+
+    Run-NavIdeCommand -SetupParameters $SetupParameters `
+                        -BranchSettings $BranchSettings `
+                        -Command $command `
+                        -LogFile $logFile `
+                        -ErrText "Error while importing from $(Split-Path $Path)" `
+                        -Verbose:$VerbosePreference
+
+          
     UnLoad-ModelTools
 }
