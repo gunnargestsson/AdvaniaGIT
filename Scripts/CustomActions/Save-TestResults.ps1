2100c3e6e54e9cd8bb64639a73f04e13bb753923
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-18 16:26:00 +0000

    Save Tests results to trx file

--- Scripts/CustomActions/Save-TestResults.ps1
+++ Scripts/CustomActions/Save-TestResults.ps1
@@ -5,8 +5,8 @@ if ($SetupParameters.testCompanyName) {
     $companyName = Get-FirstCompanyName -SQLServer (Get-DatabaseServer -BranchSettings $BranchSettings) -SQLDb $BranchSettings.databaseName
 }
 $ResultTableName = Get-DatabaseTableName -CompanyName $companyName -TableName 'CAL Test Result'
-if ($env:bamboo_buildResultKey) {
-    $OutFile = Join-Path $env:bamboo_build_working_directory "$($env:bamboo_buildResultKey).trx"
+if ($SetupParameters.TestResultsPath) {
+    $OutFile = Join-Path $Repository $SetupParameters.TestResultsPath
 } else {
     $OutFile = Join-Path $LogPath ((Split-Path $SetupParameters.LogPath -Leaf) + ".trx")
 }
