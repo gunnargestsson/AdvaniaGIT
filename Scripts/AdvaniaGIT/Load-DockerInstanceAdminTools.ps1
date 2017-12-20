3f17860e8535062ed81657bf3123d591316d39a2
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-20 10:22:05 +0000

    Add search for the service tier folder

--- Scripts/AdvaniaGIT/Load-DockerInstanceAdminTools.ps1
+++ Scripts/AdvaniaGIT/Load-DockerInstanceAdminTools.ps1
@@ -5,7 +5,8 @@
         [System.Management.Automation.Runspaces.PSSession]$Session
     )
     
-    if (!(Get-Module -Name Microsoft.Dynamics.Nav.Management)) {      
+    if (!(Get-Module -Name Microsoft.Dynamics.Nav.Management)) { 
+      $serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service").FullName     
       Import-Module (Join-Path $serviceTierFolder 'Microsoft.Dynamics.Nav.Management.psm1') -DisableNameChecking -Global -ErrorAction SilentlyContinue
     }
 }
