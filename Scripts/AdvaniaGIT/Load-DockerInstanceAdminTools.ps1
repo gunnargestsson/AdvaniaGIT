908af7cf1bcfac4f4c65975a414110ebd635919b
Gunnar Gestsson <gunnar@dynamics.is>
2017-12-20 10:51:19 +0000

    Bugfix for load docker instance admin tools

--- Scripts/AdvaniaGIT/Load-DockerInstanceAdminTools.ps1
+++ Scripts/AdvaniaGIT/Load-DockerInstanceAdminTools.ps1
@@ -5,8 +5,9 @@
         [System.Management.Automation.Runspaces.PSSession]$Session
     )
     
-    if (!(Get-Module -Name Microsoft.Dynamics.Nav.Management)) { 
-      $serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service").FullName     
-      Import-Module (Join-Path $serviceTierFolder 'Microsoft.Dynamics.Nav.Management.psm1') -DisableNameChecking -Global -ErrorAction SilentlyContinue
+    Invoke-Command -Session $Session -ScriptBlock {
+        if (!(Get-Module -Name Microsoft.Dynamics.Nav.Management)) { 
+          Import-Module (Join-Path $serviceTierFolder 'Microsoft.Dynamics.Nav.Management.psm1') -DisableNameChecking -Global -ErrorAction SilentlyContinue
+        }
     }
 }
