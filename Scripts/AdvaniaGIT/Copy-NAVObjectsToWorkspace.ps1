5bbf87cd03cf3b48674052937a8407f22b8070f9
Gunnar Gestsson <gunnar@dynamics.is>
2018-01-05 14:41:29 +0000

    Revert "Support Host Import of NAV object files when running docker containers"
    This reverts commit 55b45e312421c47d2c2c20c358419d07ec6ab33c.

--- Scripts/AdvaniaGIT/Copy-NAVObjectsToWorkspace.ps1
+++ Scripts/AdvaniaGIT/Copy-NAVObjectsToWorkspace.ps1
@@ -4,19 +4,13 @@
     param (
         [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
         [PSObject]$SetupParameters,
-        [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
-        [PSObject[]]$ObjectList,
-        [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
-        [Switch]$AllObjects
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [PSObject[]]$ObjectList
         )
     $ObjectsPath = (Join-Path $SetupParameters.workFolder 'Objects')
     Remove-Item -Path $ObjectsPath -Recurse -ErrorAction SilentlyContinue
     New-Item -Path $ObjectsPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-    if ($AllObjects) {
-        Copy-Item -Path (Join-Path $SetupParameters.ObjectsPath '*.txt') -Destination $ObjectsPath
-    } else {
-        foreach ($object in $ObjectList | Where-Object -Property DirectoryName -IEQ $SetupParameters.ObjectsPath) {
-            Copy-Item -Path $object.FullName -Destination $ObjectsPath
-        }
+    foreach ($object in $ObjectList | Where-Object -Property DirectoryName -IEQ $SetupParameters.ObjectsPath) {
+        Copy-Item -Path $object.FullName -Destination $ObjectsPath
     }
 }
--- Scripts/CustomActions/ImportFrom-GITtoNAV.ps1
+++ Scripts/CustomActions/ImportFrom-GITtoNAV.ps1
@@ -1,6 +1,29 @@
 Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings 
-
-& (Join-Path $PSScriptRoot 'Prepare-GITObjectsForImport.ps1')
-& (Join-Path $PSScriptRoot 'ImportFrom-WorkspaceToNAV.ps1')
-& (Join-Path $PSScriptRoot 'Compile-ImportedNAVObjects.ps1')
-
+if ($BranchSettings.dockerContainerId -gt "") {
+    Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
+} else {    
+    Load-ModelTools -SetupParameters $SetupParameters
+    $ObjectsPath = Build-Solution -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $SetupParameters.ObjectsPath
+    $lastNAVCommitId = Get-NAVLastCommitId -BranchSettings $BranchSettings
+    if ($lastNAVCommitId -gt '') {
+        $ObjectList = Get-GitModifiedFiles -GitCommitId $lastNAVCommitId
+        if ($ObjectList -ne $null) {
+            Copy-NAVObjectsToWorkspace -SetupParameters $SetupParameters -ObjectList $ObjectList
+            Update-NAVApplicationFromTxt -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath (Join-Path $SetupParameters.workFolder 'Objects') -SkipDeleteCheck
+        }
+    } elseif ($SetupParameters.objectProperties -eq "false") {
+        & (Join-Path $PSScriptRoot 'Export-GITtoSource.ps1')
+        Write-Host "Importing All objects..."
+        $command = "UPDATE [dbo].[Object] SET [Version List] = '#DELETED' WHERE [ID] < 2000000004"
+        Get-SQLCommandResult -Server (Get-DatabaseServer -BranchSettings $BranchSettings) -Database $BranchSettings.databaseName -Command $command | Out-Null
+        Import-NAVApplicationGITObject -SetupParameters $SetupParameters -BranchSettings $BranchSettings -Path (Join-Path $SetupParameters.WorkFolder "Source.txt") -ImportAction Overwrite -SynchronizeSchemaChanges Force 
+    } else {
+        Update-NAVApplicationFromTxt -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $ObjectsPath -MarkToDelete
+    }    
+    Compile-UncompiledObjects -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+    Import-PermissionSets -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+    $lastCommitIDd = Get-GitLastCommitId
+    if ($lastCommitIDd -gt '') {
+        Set-NAVLastCommitId -BranchSettings $BranchSettings -LastCommitID (Get-GitLastCommitId)
+    }
+}
