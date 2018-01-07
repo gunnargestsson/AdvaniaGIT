893c4acdbecce314dcd54c7c0f873f5d974b224a
Gunnar Gestsson <gunnar@dynamics.is>
2018-01-07 15:25:07 +0000

    Clean for DockerSettings.config

---
+++ Scripts/AdvaniaGIT/Remove-NAVDockerClientFolders.ps1
@@ -0,0 +1,12 @@
+﻿function Remove-NAVDockerClientFolders
+{
+    $DockerSettings = Get-DockerSettings
+    $newClientFolders = @()
+    foreach ($ClientFolder in $DockerSettings.ClientFolders) {
+        if (Test-Path $ClientFolder.clientFolderPath) {        
+            $newClientFolders += $ClientFolder
+        }
+    }
+    $DockerSettings.ClientFolders = $newClientFolders
+    Update-DockerSettings -DockerSettings $DockerSettings   
+}
--- Scripts/CustomActions/Delete-OldLogs.ps1
+++ Scripts/CustomActions/Delete-OldLogs.ps1
@@ -1,4 +1,8 @@
 ﻿Get-Childitem -Path (Split-Path $SetupParameters.LogPath -Parent) |
     Where-Object {$_.LastWriteTime -lt (Get-Date).AddDays(-7)} | 
     Remove-Item -Recurse
+
+Remove-NAVDockerClientFolders 
+
 Write-Host "Files and folder older than seven days have been deleted from $(Split-Path $SetupParameters.LogPath -Parent)..."
+
