9fff2d8ed82f96e7154fd8680ae8b9ab0af170fb
Gunnar Gestsson <gunnar@dynamics.is>
2018-01-07 15:39:38 +0000

    Copy file content - to fix the utf-8 issue in merge process

---
+++ Scripts/AdvaniaGIT/Copy-NAVObjectFileContent.ps1
@@ -0,0 +1,24 @@
+﻿Function Copy-NAVObjectFileContent
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [String]$Path,
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [String]$Destination,
+        [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
+        [Switch]$Force
+    )
+
+    $objectFiles = Get-Item -Path $Path 
+    foreach ($objectFile in $objectFiles) {
+        $fileContent = Get-Content -Path $objectFile.FullName -Encoding Oem 
+        $destinationFile = Join-Path $Destination $objectFile.Name
+        if ($Force) {
+            Set-Content -Path $destinationFile -Value $fileContent -Encoding Oem -Force
+        } else {
+            Set-Content -Path $destinationFile -Value $fileContent -Encoding Oem
+        }
+    }
+}
--- Scripts/CustomActions/Prepare-NAVSolutionBuild.ps1
+++ Scripts/CustomActions/Prepare-NAVSolutionBuild.ps1
@@ -26,7 +26,7 @@ git.exe clone --single-branch --branch $($SetupParameters.baseBranch) --verbose 
 $BranchFolder = Join-Path $TempFolder $SetupParameters.baseBranch
 $BranchSetupParameters = Combine-Settings (Get-Content (Join-Path $BranchFolder (Split-Path $SetupParameters.setupPath -Leaf)) | Out-String | ConvertFrom-Json) $GitSettings
 Write-Host Copying files from (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.objectsPath) '*.txt') to (Join-Path $MergeFolder 'Base') 
-Copy-Item -Path (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.objectsPath) '*.txt') -Destination (Join-Path $MergeFolder 'Base') -Force
+Copy-NAVObjectFileContent -Path (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.objectsPath) '*.txt') -Destination (Join-Path $MergeFolder 'Base') -Force
 
 # Clone the delta branches
 $DeltaFolderIndexNo = 10000
@@ -41,7 +41,7 @@ if ($SetupParameters.deltaBranchList) {
         $branchMergeFolder = (Join-Path (Join-Path $MergeFolder 'Deltas') ($DeltaFolderIndexNo.ToString() + $deltaBranch))
         New-Item $branchMergeFolder -ItemType Directory | Out-Null
         Write-Host Copying files from (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.deltasPath) '*.delta') to $branchMergeFolder 
-        Copy-Item -Path (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.deltasPath) '*.delta') -Destination $branchMergeFolder -Force
+        Copy-NAVObjectFileContent -Path (Join-Path (Join-Path $BranchFolder $BranchSetupParameters.deltasPath) '*.delta') -Destination $branchMergeFolder -Force
         $DeltaFolderIndexNo += 10
     }
 }
@@ -51,7 +51,7 @@ Write-Host Get deltas from Customer
 $branchMergeFolder = (Join-Path (Join-Path $MergeFolder 'Deltas') ($DeltaFolderIndexNo.ToString() + $sourcebranch))
 New-Item $branchMergeFolder -ItemType Directory | Out-Null
 Write-Host Copying files from (Join-Path $SetupParameters.deltasPath '*.delta') to $branchMergeFolder 
-Copy-Item -Path (Join-Path $SetupParameters.deltasPath '*.delta') -Destination $branchMergeFolder -Force
+Copy-NAVObjectFileContent -Path (Join-Path $SetupParameters.deltasPath '*.delta') -Destination $branchMergeFolder -Force
 $DeltaFolderIndexNo += 10
 
 # Back to Workfolder and clean GIT folder
