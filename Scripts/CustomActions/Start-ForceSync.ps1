6986d7df005ec2e7b0c03b57c9d2935f17ef9eaf
Gunnar Gestsson <gunnar@dynamics.is>
2018-01-08 11:11:37 +0000

    Bugfix for Sync function

--- Scripts/CustomActions/Start-ForceSync.ps1
+++ Scripts/CustomActions/Start-ForceSync.ps1
@@ -2,9 +2,9 @@
     Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
 } else {    
     Load-InstanceAdminTools -SetupParameters $SetupParameters
-    foreach ($Tenant in Get-NAVTenant -ServerInstance $InstanceName) {
+    foreach ($Tenant in Get-NAVTenant -ServerInstance $BranchSettings.instanceName) {
         Write-Host "Running Force Sync for $($Tenant.Id)..."
-        Sync-NAVTenant -ServerInstance $InstanceName -Tenant $Tenant.Id -Mode ForceSync -Force
+        Sync-NAVTenant -ServerInstance $BranchSettings.instanceName -Tenant $Tenant.Id -Mode ForceSync -Force
     }
     UnLoad-InstanceAdminTools
 }
