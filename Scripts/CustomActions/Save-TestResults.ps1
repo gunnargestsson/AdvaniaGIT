34dc05d5036cdd70aa081f20e679c9ac728a5816
Gunnar Gestsson <gunnar@dynamics.is>
2018-01-11 10:59:08 +0000

    Create a folder in repository for test results if needed

--- Scripts/CustomActions/Save-TestResults.ps1
+++ Scripts/CustomActions/Save-TestResults.ps1
@@ -10,6 +10,9 @@ if ($SetupParameters.TestResultsPath) {
 } else {
     $OutFile = Join-Path $LogPath ((Split-Path $SetupParameters.LogPath -Leaf) + ".trx")
 }
+if (!(Test-Path (Split-Path -Path $OutFile -Parent))) {
+    New-Item -Path (Split-Path -Path $OutFile -Parent) -ItemType Directory | Out-Null
+}
 Save-NAVTestResultTrx -SQLServer (Get-DatabaseServer -BranchSettings $BranchSettings) -SQLDb $BranchSettings.databaseName -ResultTableName $ResultTableName -OutFile $OutFile 
 
 if ($SetupParameters.BuildMode) {
