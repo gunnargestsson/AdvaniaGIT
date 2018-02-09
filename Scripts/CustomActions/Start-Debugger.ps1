9ebebec345fd71d60491eaa8109c59a83846c03c
Gunnar Gestsson <gunnar@dynamics.is>
2018-02-09 12:34:17 +0000

    Add support for Debugging server instance on a remote computer

--- Scripts/CustomActions/Start-Debugger.ps1
+++ Scripts/CustomActions/Start-Debugger.ps1
@@ -1,31 +1,8 @@
 Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings
-
-if ($BranchSettings.dockerContainerId -gt "") {
-    $clientPath = Copy-DockerNAVClient -SetupParameters $SetupParameters -BranchSettings $BranchSettings
-    $clientexe = Join-Path $clientPath 'Microsoft.Dynamics.Nav.Client.exe'
-    $clientSettingsPath = Join-Path $clientPath 'ClientUserSettings.config'
-    [xml]$clientUserSettings = Get-Content -Path (Join-Path $SetupParameters.LogPath 'ClientUserSettings.config')
-    Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'Server' -NewValue $BranchSettings.dockerContainerName
-} else {    
-    $clientexe = (Join-Path $SetupParameters.navIdePath 'Microsoft.Dynamics.Nav.Client.exe')
-    $clientSettingsPath = (Join-Path $SetupParameters.LogPath 'ClientUserSettings.config')
-    [xml]$clientUserSettings = Get-Content -Path (Join-Path $env:ProgramData ('Microsoft\Microsoft Dynamics NAV\' + $SetupParameters.mainVersion + '\ClientUserSettings.config'))
-    Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'Server' -NewValue $env:COMPUTERNAME
-}
-
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'ClientServicesPort' -NewValue $BranchSettings.clientServicesPort
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'ServerInstance' -NewValue $BranchSettings.instanceName
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'UrlHistory' -NewValue ""
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'TenantId' -NewValue ""
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'ClientServicesCredentialType' -NewValue Windows
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'ServicesCertificateValidationEnabled' -NewValue false
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'ServicePrincipalNameRequired' -NewValue false
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'HelpServer' -NewValue (Get-HelpServer -mainVersion $SetupParameters.mainVersion)
-Edit-NAVClientUserSettings -ClientUserSettings $clientUserSettings -KeyName 'HelpServerPort' -NewValue (Get-HelpServerPort -mainVersion $SetupParameters.mainVersion)
-Set-Content -Path $clientSettingsPath -Value $clientUserSettings.OuterXml -Force
+$ClientSettings = Prepare-NAVClient -SetupParameters $SetupParameters -BranchSettings $BranchSettings 
 
 $params = @()
-$params += @('-settings:"' + $clientSettingsPath + '"')
+$params += @('-settings:"' + $($ClientSettings.Config) + '"')
 $params += " `"DynamicsNAV://///debug`""
-Write-Host "Running: `"$clientexe`" $params" -ForegroundColor Green
-Start-Process -FilePath $clientexe -ArgumentList $params
+Write-Host "Running: `"$($ClientSettings.Client)`" $params" -ForegroundColor Green
+Start-Process -FilePath $ClientSettings.Client -ArgumentList $params
