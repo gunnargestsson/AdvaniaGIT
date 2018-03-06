9d401929de29941957f148d1c1884b53aa29916b
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-03-06 10:01:29 +0000

    New function to enable Graph API on Docker container

---
+++ Scripts/AdvaniaGIT/Enable-NAVGraphAPI.ps1
@@ -0,0 +1,21 @@
+﻿Function Enable-NAVGraphAPI
+{
+    param (
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [System.Management.Automation.Runspaces.PSSession]$Session,
+        [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
+        [Switch]$RestartServiceTier
+    )
+    
+    Invoke-Command -Session $Session -ScriptBlock {
+        param([Switch]$RestartServiceTier)
+        if (!(Get-Module -Name Microsoft.Dynamics.Nav.Management)) { 
+          Import-Module (Join-Path $serviceTierFolder 'Microsoft.Dynamics.Nav.Management.psm1') -DisableNameChecking -Global -ErrorAction SilentlyContinue
+        }
+        $ServerInstance = (Get-NAVServerInstance).ServerInstance.Split('$')[1]
+        Set-NAVServerConfiguration -ServerInstance $ServerInstance -KeyName ApiServicesEnabled -KeyValue true
+        if ($RestartServiceTier) {
+            Set-NAVServerInstance -ServerInstance $ServerInstance -Restart
+        }
+    } -ArgumentList $RestartServiceTier
+}
---
+++ Scripts/CustomActions/Start-GraphApiOnContainer.ps1
@@ -0,0 +1,9 @@
+﻿Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+if ($BranchSettings.dockerContainerId -gt "") {
+    Write-Host "Restarting Server Instance on Docker to enable Graph API..."
+    $Session = New-DockerSession -DockerContainerId $BranchSettings.dockerContainerId
+    Enable-NAVGraphAPI -Session $Session 
+    Remove-PSSession -Session $Session    
+} else {    
+    Write-Host -ForegroundColor Red "Function only available on Docker Container!"
+}
