4efa3ffa921494c749e6cfe3e8ea3d7233a8d2fa
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-03-28 13:53:39 +0000

    Bugfixes for Xlf handling

--- Scripts/AdvaniaGIT/Apply-NAVTranslationTableToXlfFile.ps1
+++ Scripts/AdvaniaGIT/Apply-NAVTranslationTableToXlfFile.ps1
@@ -17,28 +17,43 @@
     $Xlf.DocumentElement.file.SetAttribute('target-language',$TargetLanguage)
     $NoOfAlreadyTranslated = 0
     $NoOfAutomaticallyTranslated = 0
-    $NoOfSetEqualToSource = 0
+    $NoOfNeedsTranslation = 0
+    $NoOfNotToBeTranslated = 0
     foreach ($node in $Xlf.xliff.file.body.group.'trans-unit') {
-        if ($node.target) {
-            Write-Verbose "$($node.source) already translated"
-            $NoOfAlreadyTranslated ++
-        } else {
-            if ($TranslationTable.ContainsKey($node.source)) {
-                Write-Verbose "$($node.source) automatically translated"
-                $target = $Xlf.CreateElement('target', $Xlf.DocumentElement.NamespaceURI)
-                $target.InnerText = $TranslationTable.Item($node.source)
-                $targetNode = $node.InsertBefore($target,$node.note[0])
-                $NoOfAutomaticallyTranslated ++
+        if ($node.translate -ieq 'no') {
+            Write-Verbose "$($node.source) should not be translated"
+            $NoOfNotToBeTranslated ++
+        } elseif ($node.translate -ieq 'yes') {
+            if ($node.target) {
+                Write-Verbose "$($node.source) already translated"
+                $NoOfAlreadyTranslated ++
             } else {
-                Write-Verbose "no translation found for $($node.source)"
-                $target = $Xlf.CreateElement('target', $Xlf.DocumentElement.NamespaceURI)
-                $target.InnerText = $node.source
-                $targetNode = $node.InsertBefore($target,$node.note[0])
-                $NoOfSetEqualToSource ++
+                if ($TranslationTable.ContainsKey($node.source)) {
+                    Write-Verbose "$($node.source) automatically translated"
+                    $target = $Xlf.CreateElement('target', $Xlf.DocumentElement.NamespaceURI)
+                    $target.InnerText = $TranslationTable.Item($node.source)
+                    $target.SetAttribute('state','translated')
+                    $targetNode = $node.InsertBefore($target,$node.note[0])
+                    $NoOfAutomaticallyTranslated ++
+                } elseif ($Xlf.DocumentElement.file.'source-language'.Substring(0,2) -eq $TargetLanguage.Substring(0,2)) {
+                    Write-Verbose "$($node.source) automatically translated"
+                    $target = $Xlf.CreateElement('target', $Xlf.DocumentElement.NamespaceURI)
+                    $target.InnerText = $node.source
+                    $target.SetAttribute('state','translated')
+                    $targetNode = $node.InsertBefore($target,$node.note[0])
+                    $NoOfAutomaticallyTranslated ++
+                } else {
+                    Write-Verbose "no translation found for $($node.source)"
+                    $target = $Xlf.CreateElement('target', $Xlf.DocumentElement.NamespaceURI)
+                    $target.InnerText = ''
+                    $target.SetAttribute('state','needs-translation')
+                    $targetNode = $node.InsertBefore($target,$node.note[0])
+                    $NoOfNeedsTranslation ++
+                }
             }
         }
     }
-    Write-Host " Already translated: $NoOfAlreadyTranslated `r`n Automatically translated: $NoOfAutomaticallyTranslated `r`n Set equal to source: $NoOfSetEqualToSource"
+    Write-Host " Already translated: $NoOfAlreadyTranslated `r`n Automatically translated: $NoOfAutomaticallyTranslated `r`n Needs translation: $NoOfNeedsTranslation `r`n Not to be translated: $NoOfNotToBeTranslated"
     Write-Host "Saving translation file ${XlfFile}..."
     $xlf.Save($XlfFile)
 }
--- Scripts/AdvaniaGIT/Get-NAVTranslationTableFromXlf.ps1
+++ Scripts/AdvaniaGIT/Get-NAVTranslationTableFromXlf.ps1
@@ -13,9 +13,15 @@
     if (!$TranslateTable) {$TranslateTable = @{}}
     [xml]$Xlf = Get-Content -Path $XlfFile -Encoding UTF8 
     foreach ($node in $Xlf.xliff.file.body.group.'trans-unit') {
-        if ($node.target) {
-            if (!($TranslateTable.ContainsKey($node.source))) {
-                $TranslateTable.Add($node.source,$node.target)
+        if ($node.translate -ieq 'yes') {
+            if ($node.target) {
+                if (!($TranslateTable.ContainsKey($node.source))) {
+                    if ($node.target.GetType().Name -eq "String") {
+                        $TranslateTable.Add($node.source,$node.target)
+                    } else {
+                        $TranslateTable.Add($node.source,$node.target.'#text')
+                    }
+                }
             }
         }
     }
