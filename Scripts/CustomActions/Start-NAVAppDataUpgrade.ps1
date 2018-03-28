7bd466e3066444183c3ee1df6977f89bee29d41b
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-03-28 15:50:42 +0000

    Bugfix

--- Scripts/CustomActions/Start-NAVAppDataUpgrade.ps1
+++ Scripts/CustomActions/Start-NAVAppDataUpgrade.ps1
@@ -2,6 +2,8 @@
     Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
 } else {    
     Load-InstanceAdminTools -SetupParameters $SetupParameters
+    Load-InstanceAppTools -SetupParameters $SetupParameters
     Get-NAVTenant -ServerInstance $BranchSettings.instanceName | Start-NAVAppDataUpgrade -Force
     UnLoad-InstanceAppTools
+    UnLoad-InstanceAdminTools
 }
