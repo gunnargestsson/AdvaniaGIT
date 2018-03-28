b6dab817cd64cda0a3cc47d2a7b4a065ccc2e835
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-03-28 15:59:17 +0000

    use Apps Management Tools

--- Scripts/CustomActions/Start-NAVAppDataUpgrade.ps1
+++ Scripts/CustomActions/Start-NAVAppDataUpgrade.ps1
@@ -2,8 +2,8 @@
     Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
 } else {    
     Load-InstanceAdminTools -SetupParameters $SetupParameters
-    Load-InstanceAppTools -SetupParameters $SetupParameters
+    Load-AppsManagementTools -SetupParameters $SetupParameters
     Get-NAVTenant -ServerInstance $BranchSettings.instanceName | Start-NAVAppDataUpgrade -Force
-    UnLoad-InstanceAppTools
+    UnLoad-AppsManagementTools
     UnLoad-InstanceAdminTools
 }
