a2213ecb2819e19ca463c495ddfe1dc3c4d9f5fb
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-03-28 15:38:19 +0000

    New function to start NAV App Data Upgrade

---
+++ Scripts/CustomActions/Start-NAVAppDataUpgrade.ps1
@@ -0,0 +1,7 @@
+﻿if ($BranchSettings.dockerContainerId -gt "") {
+    Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
+} else {    
+    Load-InstanceAdminTools -SetupParameters $SetupParameters
+    Get-NAVTenant -ServerInstance $BranchSettings.instanceName | Start-NAVAppDataUpgrade -Force
+    UnLoad-InstanceAppTools
+}
