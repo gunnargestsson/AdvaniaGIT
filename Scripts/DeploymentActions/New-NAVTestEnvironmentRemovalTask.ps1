2a62c1c7eda712a05d94089dbec61ccc1521a8cb
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-04-03 15:58:30 +0000

    Add deployment action that will remove test environment after x days

---
+++ Scripts/DeploymentActions/New-NAVTestEnvironmentRemovalTask.ps1
@@ -0,0 +1,18 @@
+﻿$VMAdmin = Get-NAVPasswordStateUser -PasswordId $DeploymentSettings.NavServerPid
+$VMCredential = New-Object System.Management.Automation.PSCredential($VMAdmin.UserName, (ConvertTo-SecureString $VMAdmin.Password -AsPlainText -Force))
+
+Write-Host "Connecting to $($DeploymentSettings.instanceServer)..."
+$Session = New-NAVRemoteSession -Credential $VMCredential -HostName $DeploymentSettings.instanceServer
+
+Write-Host "Scheduling removal of instance and database for $($DeploymentSettings.instanceName) at $((Get-Date).AddDays($DeploymentSettings.daysToLive))..."
+Invoke-Command -Session $Session -ScriptBlock {
+    param([string]$instanceName,[string]$branchId,[int]$daysToLive,[string]$user,[string]$password)   
+    $command = "C:\AdvaniaGIT\Scripts\Start-DeploymentAction.ps1 -ScriptName Remove-NAVTestEnvironmentAsTask.ps1 -DeploymentSettings @{instanceName='$instanceName';branchId='$branchId'}"
+    $Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -WindowStyle Hidden -command `"$command`""
+    $Trigger = New-ScheduledTaskTrigger -At (Get-Date).AddDays($daysToLive) -Once     
+    Get-ScheduledTask -TaskName "Stop $instanceName" -ErrorAction SilentlyContinue | Unregister-ScheduledTask -Confirm:$false 
+    Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "Stop $instanceName" -Description "Clean test setup" -RunLevel Highest -User $user -Password $password -Force
+} -ArgumentList ($DeploymentSettings.instanceName, $DeploymentSettings.branchId, $DeploymentSettings.daysToLive,$VMAdmin.UserName,$VMAdmin.Password)
+
+
+$Session | Remove-PSSession
---
+++ Scripts/DeploymentActions/Remove-NAVTestEnvironmentAsTask.ps1
@@ -0,0 +1,20 @@
+﻿Write-Host "Remove instance and database for $($DeploymentSettings.instanceName)..."
+
+# Find NAV major version based on the repository NAV version - client
+if (Test-Path "$($Env:ProgramFiles)\Microsoft Dynamics NAV\*\Service") {
+    $SetupParameters | Add-Member "navServicePath" (Get-Item -Path "$($Env:ProgramFiles)\Microsoft Dynamics NAV\*\Service").FullName
+    $SetupParameters | Add-Member "mainVersion" (Split-Path (Split-Path $SetupParameters.navServicePath -Parent) -Leaf)
+    $SetupParameters | Add-Member "navRelease" (Get-NAVRelease -mainVersion (Split-Path (Split-Path $SetupParameters.navServicePath -Parent) -Leaf))
+    $SetupParameters | Add-Member "navVersion" (Get-Item -Path (Join-Path $SetupParameters.navServicePath "Microsoft.Dynamics.Nav.Server.exe")).VersionInfo.ProductVersion
+}
+if (Test-Path "$(${env:ProgramFiles(x86)})\Microsoft Dynamics NAV\*\Roletailored Client") {
+    $SetupParameters | Add-Member "navIdePath" (Get-Item -Path "$(${env:ProgramFiles(x86)})\Microsoft Dynamics NAV\*\Roletailored Client").FullName
+}
+
+$SetupParameters | Add-Member -MemberType NoteProperty -Name branchId -Value $DeploymentSettings.branchId -Force
+$BranchSettings = Get-BranchSettings -SetupParameters $SetupParameters
+if ($BranchSettings.instanceName -ieq $DeploymentSettings.instanceName) {
+    Load-InstanceAdminTools -SetupParameters $SetupParameters
+    Remove-NAVEnvironment -BranchSettings $BranchSettings
+    UnLoad-InstanceAdminTools
+}
