a1be44f01f60bbea627148df2885b743fed20924
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-04-05 19:44:27 +0000

    Add function to replace content of NAVClientInstallation

---
+++ Scripts/AdvaniaGIT/Replace-NAVClientInstallation.ps1
@@ -0,0 +1,24 @@
+﻿Function Replace-NAVClientInstallation
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [String]$NAVClientInstallationPath,
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [String]$LineContaining,
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [String]$NewInsertContent
+    )
+
+    $NAVClientInstallationContent = Get-Content -Path $NAVClientInstallationPath -Encoding UTF8
+    $NewNAVClientInstallationContent = ""
+    foreach ($line in $NAVClientInstallationContent) {
+        if ($line.contains($LineContaining)) {
+            $NewNAVClientInstallationContent += $NewInsertContent + "`r`n"
+        } else {
+            $NewNAVClientInstallationContent += $line + "`r`n"
+        }
+    }
+    Set-Content -Path $NAVClientInstallationPath -Encoding UTF8 -Value $newNAVClientInstallationContent
+}
