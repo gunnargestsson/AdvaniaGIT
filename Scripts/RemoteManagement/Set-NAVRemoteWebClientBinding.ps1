41cc5739f747b0d7c4847e9422903509a9cf4d45
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-05-17 22:52:01 +0000

    Make sure website is started with try-catch-finally loop

--- Scripts/RemoteManagement/Set-NAVRemoteWebClientBinding.ps1
+++ Scripts/RemoteManagement/Set-NAVRemoteWebClientBinding.ps1
@@ -15,10 +15,15 @@
                 $certificate = Get-ChildItem Cert:\LocalMachine\My | Where-Object {$_.Thumbprint -eq $ServicesCertificateThumbprint} 
                 $WebSite = Get-Website | Where-Object -Property Name -like "*Web Client"
                 $WebSite | Stop-Website
-                $WebSite | Get-WebBinding | Remove-WebBinding 
-                New-WebBinding -Protocol "https" -Port 443 -Name $WebSite.Name
-                (Get-WebBinding -Protocol "https" -Port 443 -Name $WebSite.Name).AddSslCertificate($ServicesCertificateThumbprint, "MY")
-                $WebSite | Start-Website
+                try {
+                    $WebSite | Get-WebBinding | Remove-WebBinding 
+                    New-WebBinding -Protocol "https" -Port 443 -Name $WebSite.Name
+                    (Get-WebBinding -Protocol "https" -Port 443 -Name $WebSite.Name).AddSslCertificate($ServicesCertificateThumbprint, "MY")
+                } 
+                catch {}
+                finally {
+                    $WebSite | Start-Website
+                }
 
             } -ArgumentList ($ServicesCertificateThumbprint)
     }    
