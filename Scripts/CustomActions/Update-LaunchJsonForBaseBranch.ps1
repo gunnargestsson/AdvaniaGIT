e097706677c85116a9bf90bf58e06030772a78d3
Gunnar Þ. Gestsson <gunnar.gestsson@advania.is>
2018-05-26 15:01:43 +0200

    Also update ALTests\.vscode\launch.json with branch information

--- Scripts/CustomActions/Update-LaunchJsonForBaseBranch.ps1
+++ Scripts/CustomActions/Update-LaunchJsonForBaseBranch.ps1
@@ -1,4 +1,13 @@
 ﻿$BaseSetupParameters = Get-BaseBranchSetupParameters -SetupParameters $SetupParameters
 $BaseBranchSettings = Get-BranchSettings -SetupParameters $BaseSetupParameters
-New-NAVAppJson -SetupParameters $SetupParameters
-Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BaseBranchSettings
+New-NAVAppJson -SetupParameters $SetupParameters 
+
+$LaunchJsonPath = Join-Path $SetupParameters.VSCodePath ".vscode\launch.json"
+if (!(Test-Path -Path (Split-Path $LaunchJsonPath -Parent))) {
+    New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
+}
+
+Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BaseBranchSettings -LaunchJsonPath $LaunchJsonPath
+
+$LaunchJsonPath = Join-Path "$($SetupParameters.VSCodePath)$(Split-Path $SetupParameters.testObjectsPath -Leaf)" ".vscode\launch.json"
+Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BaseBranchSettings -LaunchJsonPath $LaunchJsonPath
