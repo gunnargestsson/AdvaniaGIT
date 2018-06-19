252350be74152571282898d7d0cdb1241e91c988
Gunnar Þ. Gestsson <gunnar.gestsson@advania.is>
2018-06-19 20:07:31 +0000

    Bugfix for getdockeripaddress

--- Scripts/AdvaniaGIT/Get-DockerIPAddress.ps1
+++ Scripts/AdvaniaGIT/Get-DockerIPAddress.ps1
@@ -4,5 +4,6 @@
         [System.Management.Automation.Runspaces.PSSession]$Session
     )
 
-    Invoke-Command -Session $Session -ScriptBlock { (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" -and $_.IPAddress -ne "127.0.0.1" })[0].IPAddress }    
+    $IP = (Invoke-Command -Session $Session -ScriptBlock { (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq "IPv4" -and $_.IPAddress -ne "127.0.0.1" })[0].IPAddress })
+    $IP.value
 }
