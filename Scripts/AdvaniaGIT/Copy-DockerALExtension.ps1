2773839f349042debdd40a97fae9d39251935fe0
Gunnar Þ. Gestsson <gunnar.gestsson@advania.is>
2018-06-19 19:57:22 +0000

    Search for AL VS Code Addin on the C:\Run folder

--- Scripts/AdvaniaGIT/Copy-DockerALExtension.ps1
+++ Scripts/AdvaniaGIT/Copy-DockerALExtension.ps1
@@ -8,17 +8,21 @@
     $Session = New-DockerSession -DockerContainerId $BranchSettings.dockerContainerId
     Invoke-Command -Session $Session -ScriptBlock {
         param([String]$LogFolder)
-        $wwwRootPath = (Get-Item "HKLM:\SOFTWARE\Microsoft\InetStp").GetValue("PathWWWRoot")
-        $wwwRootPath = [System.Environment]::ExpandEnvironmentVariables($wwwRootPath)
-        $Source = Join-Path $wwwRootPath "http\*.vsix"
-        $Extension = Get-Item -Path $Source
+        $Extension = Get-Item -Path 'C:\Run\*.vsix'
+        if (!$Extension) {
+            $wwwRootPath = (Get-Item "HKLM:\SOFTWARE\Microsoft\InetStp").GetValue("PathWWWRoot")
+            $wwwRootPath = [System.Environment]::ExpandEnvironmentVariables($wwwRootPath)
+            $Source = Join-Path $wwwRootPath "http\*.vsix"
+            $Extension = Get-Item -Path $Source
+        }
         if ($Extension) {
             if (Test-Path -Path $Extension) {
                 Write-Host "Copying AL Extension to Host Computer..."
                 $Destination = Join-Path "C:\Host\Log" $LogFolder
                 Copy-Item -Path $Extension.FullName -Destination $Destination -Recurse -Force
             }
-        }
+        } 
+
     } -ArgumentList (Split-Path $SetupParameters.LogPath -Leaf)
     Remove-PSSession $Session
 }
