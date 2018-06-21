c1903587139c7ecf77c7ca9ffc5fe885d4593181
Gunnar Þ. Gestsson <gunnar.gestsson@advania.is>
2018-06-21 22:19:16 +0000

    Update the app installation function

--- Scripts/AdvaniaGIT/Install-NAVAppInDocker.ps1
+++ Scripts/AdvaniaGIT/Install-NAVAppInDocker.ps1
@@ -11,13 +11,19 @@
     
     $AppFolderPath = $AppFolderPath.Replace($SetupParameters.rootPath,"C:\Host")
     Invoke-Command -Session $Session -ScriptBlock {
-        param([String]$SetupParameters, [String]$AppFolderPath )
+        param([PSObject]$SetupParameters, [String]$AppFolderPath )
         if (!(Get-Module -Name Microsoft.Dynamics.Nav.Management)) { 
           Import-Module (Join-Path $serviceTierFolder 'Microsoft.Dynamics.Nav.Management.psm1') -DisableNameChecking -Global -ErrorAction SilentlyContinue
         }
+        if (!(Get-Module -Name Microsoft.Dynamics.Nav.Apps.Management)) {      
+          Import-Module (Join-Path $serviceTierFolder 'Microsoft.Dynamics.Nav.Apps.Management.psd1') -Global -DisableNameChecking -ErrorAction SilentlyContinue
+        }
+        Import-Module AdvaniaGIT -DisableNameChecking
+
         $BranchSettings = Get-BranchSettings -SetupParameters $SetupParameters 
+        
         Get-ChildItem -Path $AppFolderPath -Filter "*.app" | ForEach-Object {
-            $ext = Publish-NAVApp -ServerInstance $BranchSettings.instanceName -Path $_.FullName -PassThru
+            $ext = Publish-NAVApp -ServerInstance $BranchSettings.instanceName -Path $_.FullName -PassThru -SkipVerification
             Sync-NAVApp -ServerInstance $BranchSettings.instanceName -Name $ext.Name -Tenant default
             Install-NAVApp -ServerInstance $BranchSettings.instanceName -Name $ext.Name -Tenant default
         }
