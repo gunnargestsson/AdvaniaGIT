9bd72ed82d4ce8b3f49863af5b7246c393cf6f35
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-06-29 15:15:57 +0000

    Add support for importing multiple sets in a single file

--- Scripts/AdvaniaGIT/Import-PermissionSets.ps1
+++ Scripts/AdvaniaGIT/Import-PermissionSets.ps1
@@ -11,18 +11,22 @@
     foreach ($PermissionSet in $PermissionSets) {
         [xml]$Xml = Get-Content $PermissionSet.FullName
         Write-Host "Importing Permission Set $($PermissionSet.FullName)"
-        Insert-NAVPermissionSet -BranchSettings $BranchSettings -RoleID $Xml.PermissionSets.PermissionSet.RoleID -RoleName $Xml.PermissionSets.PermissionSet.RoleName
-        foreach ($Permission in $Xml.PermissionSets.PermissionSet.Permission) {
-            Insert-NAVPermission `
-                -BranchSettings $BranchSettings `
-                -RoleID $Xml.PermissionSets.PermissionSet.RoleID `
-                -ObjectType $Permission.ObjectType `
-                -ObjectID $Permission.ObjectID `
-                -ReadPermission $Permission.ReadPermission `
-                -InsertPermission $Permission.InsertPermission `
-                -ModifyPermission $Permission.ModifyPermission `
-                -DeletePermission $Permission.DeletePermission `
-                -ExecutePermission $Permission.ExecutePermission
+        foreach ($Sets in $Xml.PermissionSets) {
+            foreach ($Set in $Sets.PermissionSet) {
+                Insert-NAVPermissionSet -BranchSettings $BranchSettings -RoleID $Set.RoleID -RoleName $Set.RoleName
+                foreach ($Permission in $Set.Permission) {
+                    Insert-NAVPermission `
+                        -BranchSettings $BranchSettings `
+                        -RoleID $Set.RoleID `
+                        -ObjectType $Permission.ObjectType `
+                        -ObjectID $Permission.ObjectID `
+                        -ReadPermission $Permission.ReadPermission `
+                        -InsertPermission $Permission.InsertPermission `
+                        -ModifyPermission $Permission.ModifyPermission `
+                        -DeletePermission $Permission.DeletePermission `
+                        -ExecutePermission $Permission.ExecutePermission
+                }
+            }
         }    
     }
 }
