6a5f68731ef27379b6104f6dc5f4ff66d6f7ae38
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-08-16 18:03:43 +0000

    Export Test codeunits to Tests folder

--- Scripts/CustomActions/ImportFrom-ModifiedNAVtoGIT.ps1
+++ Scripts/CustomActions/ImportFrom-ModifiedNAVtoGIT.ps1
@@ -12,7 +12,10 @@ if ($BranchSettings.dockerContainerId -gt "") {
 }
 Load-ModelTools -SetupParameters $SetupParameters
 $ExportPath = Join-Path $SetupParameters.WorkFolder 'Target.txt'
+$ExportTempPath = Join-Path $SetupParameters.WorkFolder 'Objects'
 Remove-Item -Path $ExportPath -Force -ErrorAction SilentlyContinue
+Remove-Item -Path $ExportTempPath -Recurse -Force -ErrorAction SilentlyContinue
+New-Item -Path $ExportTempPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
 Export-ModifiedNAVTxtFromApplication -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $ExportPath
 Set-NAVApplicationObjectProperty -TargetPath $ExportPath -ModifiedProperty No
@@ -29,9 +32,24 @@ if ($SetupParameters.datetimeCulture -gt "" -and $SetupParameters.datetimeCultur
     New-Item -Path $TempObjectPath -ItemType Directory | Out-Null
     Split-NAVApplicationObjectFile -Source $ExportPath -Destination $TempObjectPath -Force
     Convert-NAVObjectsDateTime -FromCulture (Get-Culture).Name -ToCulture $SetupParameters.datetimeCulture -ObjectPath (Join-Path $TempObjectPath "*.TXT")
-    Copy-Item -Path (Join-Path $TempObjectPath "*.TXT") -Destination $SetupParameters.ObjectsPath -Force 
+    Copy-Item -Path (Join-Path $TempObjectPath "*.TXT") -Destination $ExportTempPath -Force 
     Remove-Item $TempObjectPath -Recurse -Force 
 } else {
-    Split-NAVApplicationObjectFile -Source $ExportPath -Destination $SetupParameters.ObjectsPath -Force
+    Split-NAVApplicationObjectFile -Source $ExportPath -Destination $ExportTempPath -Force
 }
+
+foreach ($file in (Get-ChildItem -Path $ExportTempPath -Filter *.TXT)) {
+    if (($file.BaseName).SubString(0,3) -eq "COD") {
+        $content = Get-Content -Path $file.FullName -Encoding Oem -Raw
+        if ($content.IndexOf("Subtype=Test;") -gt 0) {
+            Copy-Item -Path $file.FullName -Destination $SetupParameters.TestObjectsPath
+        } else {
+            Copy-Item -Path $file.FullName -Destination $SetupParameters.ObjectsPath
+        }
+    } else {
+        Copy-Item -Path $file.FullName -Destination $SetupParameters.ObjectsPath
+    }
+}
+
+
 UnLoad-ModelTools
