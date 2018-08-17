9e5dc58910d48220cafed34da3cd7f4e5f71bf3d
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-08-17 20:55:15 +0000

    Bugfixes and new test options

--- Scripts/RemoteManagement/Get-NAVRemoteConfig.ps1
+++ Scripts/RemoteManagement/Get-NAVRemoteConfig.ps1
@@ -6,34 +6,33 @@
         [Switch]$Initial
     )
 
-    if ($Initial) {
+    if ($Initial -or [String]::IsNullOrEmpty($Global:SettingsFilePath)) {
         $Configs = Get-ChildItem -Path (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) $SettingsFilePath)
         if ($Configs.count -eq 1) {
-            $SettingsFilePath = $Configs[0].FullName
+            $ConfigPath = $Configs[0].FullName
         } else {
             $index = 1
-            foreach ($Config in $Configs) {
-                $Config | Add-Member -MemberType NoteProperty -Name No -Value $index
+            foreach ($ConfigPath in $Configs) {
+                $ConfigPath | Add-Member -MemberType NoteProperty -Name No -Value $index
                 $index += 1
             }
 
-
-        # Start Menu
-        Clear-Host
-        For ($i=0; $i -le 10; $i++) { Write-Host "" }
-        $Configs | Format-Table -Property No, BaseName -AutoSize | Out-Host
-        $input = Read-Host "Please select Configuration configuration (0 = cancel)"
-        switch ($input) {
-            '0' { throw }
-            default { $SettingsFilePath = $Configs | Where-Object -Property No -EQ $input }
+            # Start Menu
+            Clear-Host
+            For ($i=0; $i -le 10; $i++) { Write-Host "" }
+            $Configs | Format-Table -Property No, BaseName -AutoSize | Out-Host
+            $input = Read-Host "Please select Configuration configuration (0 = cancel)"
+            switch ($input) {
+                '0' { throw }
+                default { $ConfigPath = $Configs | Where-Object -Property No -EQ $input }
             }
         }        
     } else {
-        $SettingsFilePath = $Global:SettingsFilePath
+        $ConfigPath = $Global:SettingsFilePath
     }
-    if (Test-Path -Path $SettingsFilePath) {
-        $Global:SettingsFilePath = $SettingsFilePath
-        $Config = Get-Content -Path $SettingsFilePath | Out-String | ConvertFrom-Json
+    if (Test-Path -Path $ConfigPath) {
+        $Global:SettingsFilePath = $ConfigPath
+        $Config = Get-Content -Path $ConfigPath | Out-String | ConvertFrom-Json
         return $Config
     } else {
         Write-Host "Remote configuration not found!"
