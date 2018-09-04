c2eeacf8738dccf0e5ec8759c4b185d1a1f89870
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-09-04 16:24:02 +0000

    New build function to collect alpackages into Artifacts

---
+++ Scripts/CustomActions/Copy-ALPackagesToArtifactStagingDirectory.ps1
@@ -0,0 +1,5 @@
+﻿if ($SetupParameters.BuildMode) {
+    $BranchWorkFolder = Join-Path $SetupParameters.WorkFolder $SetupParameters.branchId
+    New-Item -Path (Join-Path $SetupParameters.repository 'Artifacts') -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null 
+    Copy-Item -Path (Join-Path $BranchWorkFolder 'Symbols\*.*') -Destination (Join-Path $Repository 'Artifacts')
+}    
