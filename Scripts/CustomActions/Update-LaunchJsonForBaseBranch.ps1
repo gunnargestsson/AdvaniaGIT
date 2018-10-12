a2e7745bd99749d9d11e0c24356ddf63847bd6e8
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-10-12 22:02:05 +0000

    Verify ALTests path for launch.json

--- Scripts/CustomActions/Update-LaunchJsonForBaseBranch.ps1
+++ Scripts/CustomActions/Update-LaunchJsonForBaseBranch.ps1
@@ -3,11 +3,23 @@ $BaseBranchSettings = Get-BranchSettings -SetupParameters $BaseSetupParameters
 New-NAVAppJson -SetupParameters $SetupParameters 
 
 $LaunchJsonPath = Join-Path $SetupParameters.VSCodePath ".vscode\launch.json"
+if (!(Test-Path -Path (Split-Path (Split-Path $LaunchJsonPath -Parent) -Parent))) {
+    New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
+}
 if (!(Test-Path -Path (Split-Path $LaunchJsonPath -Parent))) {
     New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
 }
 
 Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BaseBranchSettings -LaunchJsonPath $LaunchJsonPath
 
-$LaunchJsonPath = Join-Path "$($SetupParameters.VSCodePath)$(Split-Path $SetupParameters.testObjectsPath -Leaf)" ".vscode\launch.json"
-Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BaseBranchSettings -LaunchJsonPath $LaunchJsonPath
+$ALTestPath = Join-Path $SetupParameters.VSCodePath $(Split-Path $SetupParameters.testObjectsPath -Leaf)
+if (Test-Path -Path $ALTestPath) {
+    $LaunchJsonPath = Join-Path $ALTestPath ".vscode\launch.json"
+    if (!(Test-Path -Path (Split-Path (Split-Path $LaunchJsonPath -Parent) -Parent))) {
+        New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
+    }
+    if (!(Test-Path -Path (Split-Path $LaunchJsonPath -Parent))) {
+        New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
+    }
+    Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BaseBranchSettings -LaunchJsonPath $LaunchJsonPath
+}
--- Scripts/CustomActions/Update-LaunchJsonForCurrentBranch.ps1
+++ Scripts/CustomActions/Update-LaunchJsonForCurrentBranch.ps1
@@ -1,6 +1,9 @@
 New-NAVAppJson -SetupParameters $SetupParameters
 
 $LaunchJsonPath = Join-Path $SetupParameters.VSCodePath ".vscode\launch.json"
+if (!(Test-Path -Path (Split-Path (Split-Path $LaunchJsonPath -Parent) -Parent))) {
+    New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
+}
 if (!(Test-Path -Path (Split-Path $LaunchJsonPath -Parent))) {
     New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
 }
@@ -10,5 +13,12 @@ Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BranchSe
 $ALTestPath = Join-Path $SetupParameters.VSCodePath $(Split-Path $SetupParameters.testObjectsPath -Leaf)
 if (Test-Path -Path $ALTestPath) {
     $LaunchJsonPath = Join-Path $ALTestPath ".vscode\launch.json"
+    if (!(Test-Path -Path (Split-Path (Split-Path $LaunchJsonPath -Parent) -Parent))) {
+    New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
+    }
+    if (!(Test-Path -Path (Split-Path $LaunchJsonPath -Parent))) {
+        New-Item -Path (Split-Path $LaunchJsonPath -Parent) -ItemType Directory | Out-Null
+    }
+
     Update-NAVLaunchJson -SetupParameters $SetupParameters -BranchSettings $BranchSettings -LaunchJsonPath $LaunchJsonPath
 }
