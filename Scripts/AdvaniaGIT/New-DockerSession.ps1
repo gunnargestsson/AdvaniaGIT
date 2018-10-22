f8465bba0d4a178c11f4d88a2391656c73358feb
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-10-22 16:41:25 +0000

    Add support for installed Business Central

--- Scripts/AdvaniaGIT/New-DockerSession.ps1
+++ Scripts/AdvaniaGIT/New-DockerSession.ps1
@@ -7,6 +7,9 @@
     $Session = New-PSSession -ContainerId $DockerContainerId -RunAsAdministrator    
     Invoke-Command -Session $Session -ScriptBlock {
         $serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics NAV\*\Service").FullName
+        if ($serviceTierFolder -eq $null) {
+        	$serviceTierFolder = (Get-Item "C:\Program Files\Microsoft Dynamics 365 Business Central\*\Service").FullName
+        }
         . (Join-Path $env:SystemDrive "Run\HelperFunctions.ps1")
     }
 
