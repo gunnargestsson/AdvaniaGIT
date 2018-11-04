8d0e432fd0a59a84c414eea172488f544d18e3c8
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-11-04 14:09:38 +0000

    Update Scripts/CustomActions/Prepare-NAVUnitTest.ps1

--- Scripts/CustomActions/Prepare-NAVUnitTest.ps1
+++ Scripts/CustomActions/Prepare-NAVUnitTest.ps1
@@ -3,9 +3,12 @@
 } else {    
     Load-InstanceAdminTools -SetupParameters $SetupParameters
     $UserExists = Get-NAVServerUser -ServerInstance $BranchSettings.instanceName | Where-Object -Property UserName -Match "\w+\\$($env:USERNAME)"
-    if ($UserExists -eq $null) {
+    if (!($UserExists)) {
+        Write-Host "Creating local user..."
         New-NAVServerUser -ServerInstance $BranchSettings.instanceName -WindowsAccount $env:USERNAME
         New-NAVServerUserPermissionSet -ServerInstance $BranchSettings.instanceName -WindowsAccount $env:USERNAME -PermissionSetId SUPER
+    } else {
+        Write-Host "Local user exists..."
     }
     Initialize-NAVTestCompany -SetupParameters $SetupParameters -BranchSettings $BranchSettings -RestartService
     UnLoad-InstanceAdminTools
