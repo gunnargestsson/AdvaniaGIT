a81fbdc50defbe4391239ac7d589127ecf941bb4
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-11-04 13:01:58 +0000

    Import license inside container

--- Scripts/CustomActions/Create-NavBackup.ps1
+++ Scripts/CustomActions/Create-NavBackup.ps1
@@ -6,13 +6,18 @@ if ($BranchSettings.dockerContainerId -gt "") {
     Write-Host "Removing NAV License from database before backing up..."
     Remove-NAVDatabaseLicense -BranchSettings $BranchSettings
     Create-NAVDatabaseBackup -SetupParameters $SetupParameters -BranchSettings $BranchSettings
-    if ($SetupParameters.LicenseFilePath) {
-        Write-Host "Importing license file from $($SetupParameters.LicenseFilePath)..."
+    if ($SetupParameters.LicenseFilePath) {        
         if (Test-Path $SetupParameters.LicenseFilePath) {  
+            Write-Host "Importing license file from $($SetupParameters.LicenseFilePath)..."
             Load-InstanceAdminTools -SetupParameters $SetupParameters
             Update-NAVLicense -BranchSettings $BranchSettings -LicenseFilePath $SetupParameters.LicenseFilePath
             UnLoad-InstanceAdminTools
-        }
+        } elseif (Test-Path "c:\run\my\license.flf") {
+            Write-Host "Importing license file from c:\run\my\license.flf..."
+            Load-InstanceAdminTools -SetupParameters $SetupParameters
+            Update-NAVLicense -BranchSettings $BranchSettings -LicenseFilePath "c:\run\my\license.flf"
+            UnLoad-InstanceAdminTools
+        } 
     } else {
         Write-Host "No license file defined..."
     }
