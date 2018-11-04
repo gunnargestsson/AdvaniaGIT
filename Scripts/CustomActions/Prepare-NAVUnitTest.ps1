aeb76c406538cdebe2a81f308508fc230da76211
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-11-04 13:30:13 +0000

    Update Scripts/CustomActions/Prepare-NAVUnitTest.ps1

--- Scripts/CustomActions/Prepare-NAVUnitTest.ps1
+++ Scripts/CustomActions/Prepare-NAVUnitTest.ps1
@@ -5,7 +5,7 @@
     $UserExists = Get-NAVServerUser -ServerInstance $BranchSettings.instanceName | Where-Object -Property UserName -Match "\w+\\$($env:USERNAME)"
     if ($UserExists -eq $null) {
         New-NAVServerUser -ServerInstance $BranchSettings.instanceName -WindowsAccount $env:USERNAME
-        New-NAVServerUserPermissionSet -ServerInstance DynamicsNAV130 -WindowsAccount $env:USERNAME -PermissionSetId SUPER
+        New-NAVServerUserPermissionSet -ServerInstance $BranchSettings.instanceName -WindowsAccount $env:USERNAME -PermissionSetId SUPER
     }
     Initialize-NAVTestCompany -SetupParameters $SetupParameters -BranchSettings $BranchSettings -RestartService
     UnLoad-InstanceAdminTools
