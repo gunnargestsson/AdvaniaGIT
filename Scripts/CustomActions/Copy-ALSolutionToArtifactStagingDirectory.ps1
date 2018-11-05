ced3975bd75855799d5987c3929ec3d95bf1c622
Gunnar Þ. Gestsson <gunnar.gestsson@advania.is>
2018-11-05 19:41:08 +0000

    Updates to workfolder settings

--- Scripts/CustomActions/Copy-ALSolutionToArtifactStagingDirectory.ps1
+++ Scripts/CustomActions/Copy-ALSolutionToArtifactStagingDirectory.ps1
@@ -1,5 +1,5 @@
 ﻿if ($SetupParameters.BuildMode) {
-    $BranchWorkFolder = Join-Path $SetupParameters.WorkFolder $SetupParameters.branchId
+    $BranchWorkFolder = Join-Path $SetupParameters.rootPath "Log\$($SetupParameters.BranchId)"
     Remove-Item -Path (Join-Path $SetupParameters.repository 'Artifacts') -Recurse -Force -ErrorAction SilentlyContinue
     New-Item -Path (Join-Path $SetupParameters.repository 'Artifacts') -ItemType Directory -Force -ErrorAction SilentlyContinue | Out-Null 
     Copy-Item -Path (Join-Path $BranchWorkFolder 'out\*.*') -Destination (Join-Path $Repository 'Artifacts')
