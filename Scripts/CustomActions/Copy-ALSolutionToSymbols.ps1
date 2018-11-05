9cc4301ebd66855e54a2e5ad49890ba95a9e0788
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-11-05 14:25:07 +0000

    Update Scripts/CustomActions/Copy-ALSolutionToSymbols.ps1

--- Scripts/CustomActions/Copy-ALSolutionToSymbols.ps1
+++ Scripts/CustomActions/Copy-ALSolutionToSymbols.ps1
@@ -1,4 +1,4 @@
 ﻿if ($SetupParameters.BuildMode) {
-    $BranchWorkFolder = Join-Path $SetupParameters.WorkFolder $SetupParameters.branchId    
+    $BranchWorkFolder = Join-Path $SetupParameters.rootPath "Log\$($SetupParameters.BranchId)"
     Copy-Item -Path (Join-Path $BranchWorkFolder 'out\*.*') -Destination (Join-Path $BranchWorkFolder 'Symbols')
 }    
