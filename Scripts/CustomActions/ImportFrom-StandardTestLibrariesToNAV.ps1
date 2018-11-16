aa242ded21bb2fd95213295b88b7c4157ffee374
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-11-16 23:49:02 +0000

    Create ImportFrom-StandardTestLibrariesToNAV.ps1

---
+++ Scripts/CustomActions/ImportFrom-StandardTestLibrariesToNAV.ps1
@@ -0,0 +1,18 @@
+﻿if ($BranchSettings.dockerContainerId -gt "") {
+    if ([Bool](Get-Module NAVContainerHelper)) {
+        Import-TestToolkitToNavContainer -containerName $BranchSettings.dockerContainerName -includeTestLibrariesOnly
+    } else {
+        Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings
+    }
+} else {        
+    # Import Standard Test Tool Kit
+    if (Test-Path (Join-Path $env:SystemDrive 'TestToolKit')) {
+        Load-ModelTools -SetupParameters $SetupParameters
+        foreach ($testObjectFile in (Get-ChildItem -Path (Join-Path $env:SystemDrive 'TestToolKit\CALTestLibraries*.fob'))) {
+            Import-NAVApplicationGITObject -SetupParameters $SetupParameters -BranchSettings $BranchSettings -Path $testObjectFile.FullName -ImportAction Overwrite -SynchronizeSchemaChanges Force
+        }
+        UnLoad-ModelTools
+    }    
+}
+        
+
