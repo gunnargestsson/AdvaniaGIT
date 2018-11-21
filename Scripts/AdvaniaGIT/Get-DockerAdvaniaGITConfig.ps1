1b56d641f495fca709f83f4d22add0c27e53ad86
Gunnar Gestsson <gunnar.gestsson@advania.is>
2018-11-21 13:34:09 +0000

    Use existing Container with the same project name

---
+++ Scripts/AdvaniaGIT/Get-DockerAdvaniaGITConfig.ps1
@@ -0,0 +1,23 @@
+﻿Function Get-DockerAdvaniaGITConfig {
+    param(
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [System.Management.Automation.Runspaces.PSSession]$Session,
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [PSObject]$SetupParameters,
+        [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
+        [PSObject]$BranchSettings
+    )
+
+    $DockerSettings = Invoke-Command -Session $Session -ScriptBlock { 
+        param([PSObject]$SetupParameters, [PSObject]$BranchSettings, [String]$GeoId, [String]$LocaleName )
+        Set-ExecutionPolicy -ExecutionPolicy Unrestricted 
+        Import-Module AdvaniaGIT
+        $GITSettings = Get-GTISettings
+        $BranchSettings = Get-BranchSettings -SetupParameters $GITSettings
+        $DockerSettings = New-Object -TypeName PSObject
+        $DockerSettings | Add-Member -MemberType NoteProperty -Name GITSettings -Value $GITSettings
+        $DockerSettings | Add-Member -MemberType NoteProperty -Name BranchSettings -Value $BranchSettings
+        Return $DockerSettings
+    } -ArgumentList ($SetupParameters, $BranchSettings, (Get-WinHomeLocation).GeoId, (Get-WinSystemLocale).Name)
+    Return $DockerSettings
+}
