be5b419dedc9c14c00eb7b8657b2560a1bf7f156
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-01-14 16:19:38 +0000

    Update Install-ALExtensionsToDocker.ps1

--- Scripts/CustomActions/Install-ALExtensionsToDocker.ps1
+++ Scripts/CustomActions/Install-ALExtensionsToDocker.ps1
@@ -2,7 +2,7 @@
     $BranchWorkFolder = Join-Path $SetupParameters.rootPath "Log\$($SetupParameters.BranchId)"
     $Session = New-DockerSession -DockerContainerId $BranchSettings.dockerContainerId
         
-    if (Test-Path (Join-Path $SetupParameters.repository "Dependencies\*.app")) {}
+    if (Test-Path (Join-Path $SetupParameters.repository "Dependencies\*.app")) {
         Install-NAVAppInDocker -Session $Session -SetupParameters $SetupParameters -AppFolderPath (Join-Path $SetupParameters.repository "Dependencies")
     }
     Install-NAVAppInDocker -Session $Session -SetupParameters $SetupParameters -AppFolderPath (Join-Path $BranchWorkFolder "out")    
