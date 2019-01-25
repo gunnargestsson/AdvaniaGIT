40f91dcb7bed5b6f7726db51231f0c8c2b03dba4
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-01-25 11:08:16 +0000

    Update Create-NewBranchId.ps1

--- Scripts/CustomActions/Create-NewBranchId.ps1
+++ Scripts/CustomActions/Create-NewBranchId.ps1
@@ -1,5 +1,8 @@
 ﻿if (Test-Path $SetupParameters.SetupPath) {
     $BranchSetup = Get-Content -Path $SetupParameters.SetupPath -Encoding UTF8 | Out-String | ConvertFrom-Json
     $BranchSetup.branchId = (New-Guid)
+    if ([bool]($BranchSetup.PSObject.Properties.name -match "dockerShared")) {
+        $BranchSetup.dockerShared = ""    
+    }
     Set-Content -Path $SetupParameters.SetupPath -Encoding UTF8 -Value (ConvertTo-Json -InputObject $BranchSetup)
 }
