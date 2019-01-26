42e988199ea3926a58995fb2e6a1498c0356ab47
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-01-26 13:04:50 +0000

    Add Test Actions

---
+++ Scripts/CustomActions/Start-NextTestsExecution.ps1
@@ -0,0 +1,16 @@
+﻿Check-NAVServiceRunning -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+if ($SetupParameters.testCompanyName) {
+    $companyName = $SetupParameters.testCompanyName
+} else {
+    $companyName = Get-FirstCompanyName -SQLServer (Get-DatabaseServer -BranchSettings $BranchSettings) -SQLDb $BranchSettings.databaseName
+}
+
+& (Join-Path $PSScriptRoot Add-NAVWindowsUser.ps1) 
+Prepare-NAVTestExecution -BranchSettings $BranchSettings -CompanyName $companyName -CodeunitIdFilter $SetupParameters.codeunitIdFilter -TestExecutionContinuing
+& (Join-Path $PSScriptRoot Prepare-NAVUnitTest.ps1) 
+
+if ([bool]($SetupParameters.PSObject.Properties.name -match "testExecution") -and $SetupParameters.testExecution -ieq "Background") {
+    & (Join-Path $PSScriptRoot Start-TestCodeunit.ps1)
+} else {
+    & (Join-path $PSScriptRoot Start-TestClient.ps1)
+}
