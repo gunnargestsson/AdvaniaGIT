d3dffbd06f98f60eef79e9131a67449bb19375f9
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-02-10 11:00:11 +0000

    Update Set-DockerInBranchSetup.ps1

--- Scripts/CustomActions/Set-DockerInBranchSetup.ps1
+++ Scripts/CustomActions/Set-DockerInBranchSetup.ps1
@@ -1,16 +1,18 @@
 ﻿$SetupJson = Get-Content $SetupParameters.setupPath -Encoding UTF8 | Out-String | ConvertFrom-Json
-if ([int]$SetupParameters.navVersion.Split(".")[0] -ge 13) {
-    if ($($SetupJson.navSolution.SubString(0,2)) -eq "W1") {
-        $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "mcr.microsoft.com/businesscentral/onprem" -Force
-    } else {
-        $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "mcr.microsoft.com/businesscentral/onprem:$($SetupJson.navSolution.SubString(0,2).ToLower())" -Force
-    }
+if ([String]::IsNullOrEmpty($SetupParameters.CUBuildMode) -or $SetupParameters.CUBuildMode -eq $false) {
+    if ([int]$SetupParameters.navVersion.Split(".")[0] -ge 13) {
+        if ($($SetupJson.navSolution.SubString(0,2)) -eq "W1") {
+            $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "mcr.microsoft.com/businesscentral/onprem" -Force
+        } else {
+            $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "mcr.microsoft.com/businesscentral/onprem:$($SetupJson.navSolution.SubString(0,2).ToLower())" -Force
+        }
  
-} else  {
-    if ($($SetupJson.navSolution.SubString(0,2)) -eq "W1") {
-        $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "microsoft/dynamics-nav:$(($SetupJson.navBuild).substring(0,4))" -Force
-    } else {
-        $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "microsoft/dynamics-nav:$(($SetupJson.navBuild).substring(0,4))-$($SetupJson.navSolution.SubString(0,2).ToLower())" -Force
+    } else  {
+        if ($($SetupJson.navSolution.SubString(0,2)) -eq "W1") {
+            $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "microsoft/dynamics-nav:$(($SetupJson.navBuild).substring(0,4))" -Force
+        } else {
+            $SetupJson | Add-Member -MemberType NoteProperty -Name dockerImage -Value "microsoft/dynamics-nav:$(($SetupJson.navBuild).substring(0,4))-$($SetupJson.navSolution.SubString(0,2).ToLower())" -Force
+        }
     }
 }
 
