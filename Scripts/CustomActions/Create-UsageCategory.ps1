b5c4e27cde167ed1928e3d6a8de8d696254e12fa
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-02-11 15:44:01 +0000

    Update Create-UsageCategory.ps1

--- Scripts/CustomActions/Create-UsageCategory.ps1
+++ Scripts/CustomActions/Create-UsageCategory.ps1
@@ -12,39 +12,41 @@ foreach ($subfolder in ($SetupParameters.ObjectsPath,$SetupParameters.DeltasPath
     }
 }
 
+foreach ($ALSolution in (Get-ALPaths -SetupParameters $SetupParameters)) {
     
-$ALFiles = Get-ChildItem -Path (Join-Path $SetupParameters.Repository 'AL') -Filter *.al -Recurse
-foreach ($ALFile in $ALFiles) {
-    Write-Verbose "Handling object $ALFile.BasicName"
-    $ALFileData = (Get-Content -Path $ALFile.FullName -Encoding UTF8) -split "`r`n"   
-    $alObjectInfo = $ALFileData.Split(' ')
-    Write-Verbose "Searching MenuSuite for $($alObjectInfo[0]) $($alObjectInfo[1])"  
-    $menuSuiteItem = $menuItems | Where-Object -Property RunObjectType -ieq $alObjectInfo[0] | Where-Object -Property RunObjectId -ieq $alObjectInfo[1]
-    if ($menuSuiteItem) {
-        Write-Verbose "found MenuItem $($menuSuiteItem.Id)"
-        $ALFileOutputData = ""
-        for ($c = 0; $c -lt 2; $c++) {
-            $ALFileOutputData += $ALFileData.Item($c)
-            $ALFileOutputData += "`r`n"
+    $ALFiles = Get-ChildItem -Path $ALSolution.FullName -Filter *.al -Recurse
+    foreach ($ALFile in $ALFiles) {
+        Write-Verbose "Handling object $ALFile.BasicName"
+        $ALFileData = (Get-Content -Path $ALFile.FullName -Encoding UTF8) -split "`r`n"   
+        $alObjectInfo = $ALFileData.Split(' ')
+        Write-Verbose "Searching MenuSuite for $($alObjectInfo[0]) $($alObjectInfo[1])"  
+        $menuSuiteItem = $menuItems | Where-Object -Property RunObjectType -ieq $alObjectInfo[0] | Where-Object -Property RunObjectId -ieq $alObjectInfo[1]
+        if ($menuSuiteItem) {
+            Write-Verbose "found MenuItem $($menuSuiteItem.Id)"
+            $ALFileOutputData = ""
+            for ($c = 0; $c -lt 2; $c++) {
+                $ALFileOutputData += $ALFileData.Item($c)
+                $ALFileOutputData += "`r`n"
+            }
+            if ([bool]($menuSuiteItem.PSObject.Properties.name -match "AccessByPermission")) {
+                $ALFileOutputData += "    AccessByPermission = $($menuSuiteItem.AccessByPermission);"
+                $ALFileOutputData += "`r`n"    
+            }
+            if ([bool]($menuSuiteItem.PSObject.Properties.name -match "ApplicationArea")) {
+                $ALFileOutputData += "    ApplicationArea = $($menuSuiteItem.ApplicationArea);"
+                $ALFileOutputData += "`r`n"    
+            }
+            if ([bool]($menuSuiteItem.PSObject.Properties.name -match "DepartmentCategory")) {
+                $ALFileOutputData += "    UsageCategory = $($menuSuiteItem.DepartmentCategory);"
+                $ALFileOutputData += "`r`n"    
+            }
+            for ($c = 2; $c -lt $ALFileData.Length; $c++) {
+                $ALFileOutputData += $ALFileData.Item($c)
+                $ALFileOutputData += "`r`n"
+            }
+            Write-Host "Updated $($ALFile.Name)"
+            Set-Content -Path $ALFile.FullName -Value $ALFileOutputData -Encoding UTF8
         }
-        if ([bool]($menuSuiteItem.PSObject.Properties.name -match "AccessByPermission")) {
-            $ALFileOutputData += "    AccessByPermission = $($menuSuiteItem.AccessByPermission);"
-            $ALFileOutputData += "`r`n"    
-        }
-        if ([bool]($menuSuiteItem.PSObject.Properties.name -match "ApplicationArea")) {
-            $ALFileOutputData += "    ApplicationArea = $($menuSuiteItem.ApplicationArea);"
-            $ALFileOutputData += "`r`n"    
-        }
-        if ([bool]($menuSuiteItem.PSObject.Properties.name -match "DepartmentCategory")) {
-            $ALFileOutputData += "    UsageCategory = $($menuSuiteItem.DepartmentCategory);"
-            $ALFileOutputData += "`r`n"    
-        }
-        for ($c = 2; $c -lt $ALFileData.Length; $c++) {
-            $ALFileOutputData += $ALFileData.Item($c)
-            $ALFileOutputData += "`r`n"
-        }
-        Write-Host "Updated $($ALFile.Name)"
-        Set-Content -Path $ALFile.FullName -Value $ALFileOutputData -Encoding UTF8
     }
-}
     
+}
