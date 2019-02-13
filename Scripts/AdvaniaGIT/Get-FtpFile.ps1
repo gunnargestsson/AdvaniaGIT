f97d3ae76889520816e0adb5f5f1ea267834d770
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-02-13 21:01:44 +0000

    Update Get-FtpFile.ps1

--- Scripts/AdvaniaGIT/Get-FtpFile.ps1
+++ Scripts/AdvaniaGIT/Get-FtpFile.ps1
@@ -13,27 +13,40 @@
 )
 
 {   
-    Write-Host "Downloading $($FtpFilePath)..."
+    Write-Host "Verify $($FtpFilePath) exists..."
     # Credentials
     $FTPRequest = [System.Net.FtpWebRequest]::Create("$($Server)$($FtpFilePath)")
-    $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($User,$Pass)
-    $FTPRequest.Method = [System.Net.WebRequestMethods+FTP]::DownloadFile
+    $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($User,$Pass)   
+    $FTPRequest.Method = [System.Net.WebRequestMethods+FTP]::GetFileSize
+    try {
+        $FTPRequest.GetResponse() | Out-Null
 
-    # Don't want Binary, Keep Alive unecessary.
-    $FTPRequest.UseBinary = $False
-    $FTPRequest.KeepAlive = $False
+        Write-Host "Downloading $($FtpFilePath)..."
+        # Credentials
+        $FTPRequest = [System.Net.FtpWebRequest]::Create("$($Server)$($FtpFilePath)")
+        $FTPRequest.Credentials = New-Object System.Net.NetworkCredential($User,$Pass)   
+        $FTPRequest.Method = [System.Net.WebRequestMethods+FTP]::DownloadFile
 
-    $FTPResponse = $FTPRequest.GetResponse()
-    $ResponseStream = $FTPResponse.GetResponseStream()
-    # Create the target file on the local system and the download buffer 
-    $LocalFileFile = New-Object IO.FileStream ($LocalFilePath,[IO.FileMode]::Create) 
-    [byte[]]$ReadBuffer = New-Object byte[] 1024 
-    # Loop through the download 
-	    do { 
-		    $ReadLength = $ResponseStream.Read($ReadBuffer,0,1024) 
-		    $LocalFileFile.Write($ReadBuffer,0,$ReadLength) 
-	    } 
-	    while ($ReadLength -ne 0)
-    $FTPResponse.Close()
-    $LocalFileFile.Close()
+        # Don't want Binary, Keep Alive unecessary.
+        $FTPRequest.UseBinary = $False
+        $FTPRequest.KeepAlive = $False    
+
+        $FTPResponse = $FTPRequest.GetResponse() 
+        $ResponseStream = $FTPResponse.GetResponseStream()
+        # Create the target file on the local system and the download buffer 
+        $LocalFileFile = New-Object IO.FileStream ($LocalFilePath,[IO.FileMode]::Create) 
+        [byte[]]$ReadBuffer = New-Object byte[] 1024 
+        # Loop through the download 
+            do { 
+                $ReadLength = $ResponseStream.Read($ReadBuffer,0,1024) 
+                $LocalFileFile.Write($ReadBuffer,0,$ReadLength) 
+            } 
+            while ($ReadLength -ne 0)
+        $FTPResponse.Close()
+        $LocalFileFile.Close()
+    }
+    catch {
+        Write-Host "File not found!"
+        throw
+    }
 }
