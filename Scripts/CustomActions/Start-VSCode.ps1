e65fe548d70b9e212e772eca4c16d08193c20acc
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-03-07 16:02:19 +0000

    Update Start-VSCode

--- Scripts/CustomActions/Start-VSCode.ps1
+++ Scripts/CustomActions/Start-VSCode.ps1
@@ -1,11 +1,8 @@
 $VSCodePaths = @((Join-Path $env:ProgramFiles "Microsoft VS Code\Code.exe"); (Join-Path ${env:ProgramFiles(x86)} "Microsoft VS Code\Code.exe"))
 foreach ($VSCodePath in $VSCodePaths) {
     if (Test-Path $VSCodePath) {
-        $ALPaths = ""
-        foreach ($ALPath in (Get-ALPaths -SetupParameters $SetupParameters)) {
-           $ALPaths += "`"$($ALPath.FullName)`" "
-        }
-        Start-Process -FilePath $VSCodePath -ArgumentList "--add $($ALPaths)"
+        New-NAVAppJson -SetupParameters $SetupParameters       
+        Start-Process -FilePath $VSCodePath -ArgumentList "--add $($SetupParameters.VSCodePath)"
     }
 }
 
