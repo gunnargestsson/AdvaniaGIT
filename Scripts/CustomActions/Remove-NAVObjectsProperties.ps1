771c24ebfe6e27a9dc993a23d647e85f47f98bf5
Erik P. Ernst <epernst@dxworld.biz>
2019-03-15 15:29:30 +0100

    Added navIdePath

--- Scripts/CustomActions/Remove-NAVObjectsProperties.ps1
+++ Scripts/CustomActions/Remove-NAVObjectsProperties.ps1
@@ -1,4 +1,8 @@
-﻿$objectList = Get-GitModifiedFiles -GitCommitId (Get-GitLastCommitId)
+﻿if ($BranchSettings.dockerContainerId -gt "") {
+    $SetupParameters.navIdePath = Copy-DockerNAVClient -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+}
+
+$objectList = Get-GitModifiedFiles -GitCommitId (Get-GitLastCommitId)
 if ($objectList -ne $null) {
     Remove-NAVObjectsProperties -SetupParameters $SetupParameters -ObjectList $objectList 
 }
