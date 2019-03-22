4ecccf0e3177621676a95f88edf380d3c1542acc
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-03-22 15:34:30 +0000

    Update AL Build process

--- Scripts/CustomActions/Start-ALBuild.ps1
+++ Scripts/CustomActions/Start-ALBuild.ps1
@@ -5,15 +5,7 @@ if (![String]::IsNullOrEmpty($SetupParameters.CreateSymbols)) {
     & (Join-path $PSScriptRoot 'Build-NAVSymbolReferences.ps1')
 }
 
-Write-Host "Enabling server instance symbol reference update..."
-& (Join-path $PSScriptRoot 'Start-ALSymbolReferenceGenerationOnServer.ps1')
-
-# Import CAL objects required for symbols
-& (Join-path $PSScriptRoot 'ImportFrom-GITCALObjectsToNAV.ps1')
-
-# Compile Queries to fix symbols
-& (Join-path $PSScriptRoot 'Start-CompileQueriesOnHost.ps1')
-& (Join-path $PSScriptRoot 'Start-CompileModifiedObjectsOnHost.ps1')
+& (Join-path $PSScriptRoot 'ImportFrom-CALDependencies.ps1')
 
 Write-Host Initialize Test Company
 & (Join-path $PSScriptRoot 'Initialize-NAVCompany.ps1')
