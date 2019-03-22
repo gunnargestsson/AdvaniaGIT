ad9f5b166c4239c840a0633cf88efed17832da0e
Gunnar Gestsson <gunnar.gestsson@advania.is>
2019-03-22 08:07:30 +0000

    Add ImportFrom-CALDependencies

---
+++ Scripts/CustomActions/ImportFrom-CALDependencies.ps1
@@ -0,0 +1,9 @@
+Write-Host "Enabling server instance symbol reference update..."
+& (Join-path $PSScriptRoot 'Start-ALSymbolReferenceGenerationOnServer.ps1')
+
+# Import CAL objects required for symbols
+& (Join-path $PSScriptRoot 'ImportFrom-GITCALObjectsToNAV.ps1')
+
+# Compile Queries to fix symbols
+& (Join-path $PSScriptRoot 'Start-CompileQueriesOnHost.ps1')
+& (Join-path $PSScriptRoot 'Start-CompileModifiedObjectsOnHost.ps1')
