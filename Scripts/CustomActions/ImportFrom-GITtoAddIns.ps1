acb8475d5e3ea2a415b7af521282ca68d1b7abfc
Gunnar Thor Gestsson <gunnar@navision.guru>
2019-05-10 21:08:44 +0000

    Bugfix

--- Scripts/CustomActions/ImportFrom-GITtoAddIns.ps1
+++ Scripts/CustomActions/ImportFrom-GITtoAddIns.ps1
@@ -3,10 +3,9 @@ if ($BranchSettings.dockerContainerId -gt "") {
     $SetupParameters.navIdePath = Copy-DockerNAVClient -SetupParameters $SetupParameters -BranchSettings $BranchSettings
 }
 
-$AddInsDestPath = Join-Path $SetupParameters.navIdePath "Add-ins"
 $AddInsSrcPath = Join-Path $SetupParameters.repository "Add-ins"
 
 if (Test-Path -Path $AddInsSrcPath) {
-    Copy-Item -Path (Join-Path $AddInsSrcPath "*.*") -Destination $AddInsDestPath -Recurse
+    Copy-Item -Path $AddInsSrcPath -Destination $SetupParameters.navIdePath -Recurse -Force
 }
 
