1b5538895eda070cd8e22eaee7317c795fc7060f
Gunnar Thor Gestsson <gunnar@navision.guru>
2019-05-11 00:42:44 +0000

    Update Download-ClientAddins.ps1

--- Scripts/CustomActions/Download-ClientAddins.ps1
+++ Scripts/CustomActions/Download-ClientAddins.ps1
@@ -3,7 +3,11 @@
 }
 
 $FileName = Join-Path $SetupParameters.LogPath 'AddIns.zip'
-Download-NAVFile -Url "https://addins.navleiga.is/addins.zip" -FileName $FileName
+if ($SetupParameters.addinsUrl) {
+    Download-NAVFile -Url $SetupParameters.addinsUrl -FileName $FileName
+} else {
+    Download-NAVFile -Url "https://addins.navleiga.is/addins.zip" -FileName $FileName
+}
 $AddInsPath = Join-Path $SetupParameters.navIdePath "Add-ins\AdvaniaGIT"
 New-Item -Path $AddInsPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 Expand-Archive -LiteralPath $FileName -DestinationPath $AddInsPath -Force
