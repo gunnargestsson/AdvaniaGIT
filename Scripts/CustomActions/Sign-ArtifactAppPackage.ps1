6fb2eff677a0d784e75c58a6047a4fa704609222
Gunnar Thor Gestsson <gunnar@navision.guru>
2019-05-14 21:44:25 +0000

    Update Sign-ArtifactAppPackage.ps1

--- Scripts/CustomActions/Sign-ArtifactAppPackage.ps1
+++ Scripts/CustomActions/Sign-ArtifactAppPackage.ps1
@@ -1,14 +1,15 @@
-﻿Copy-MissingNAVSipToHost -BranchSettings $BranchSettings
+﻿if (Test-Path $SetupParameters.SigToolExecutable) {
+    Copy-MissingNAVSipToHost -BranchSettings $BranchSettings
 
-$appPackageFileNames = Get-ChildItem -Path (Join-Path $SetupParameters.repository Artifacts) -Filter "*.app"
-foreach ($appPackageFileName in $appPackageFileNames) {
-    if (Test-Path $SetupParameters.CodeSigningCertificate) {
-      Write-Host "Signing APP package $($appPackageFileName.Name)..."
-      & $($SetupParameters.SigToolExecutable) sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "$($SetupParameters.CodeSigningCertificate)" /p "$($SetupParameters.CodeSigningCertificatePassword)" "$($appPackageFileName.FullName)"
-    }
-    else
-    {
-        Write-Host -ForegroundColor Red "Code Signing Certificate not configured in GITSettings.json!"
-        throw
+    $appPackageFileNames = Get-ChildItem -Path (Join-Path $SetupParameters.repository Artifacts) -Filter "*.app"
+    foreach ($appPackageFileName in $appPackageFileNames) {
+        if (Test-Path $SetupParameters.CodeSigningCertificate) {
+          Write-Host "Signing APP package $($appPackageFileName.Name)..."
+          & $($SetupParameters.SigToolExecutable) sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "$($SetupParameters.CodeSigningCertificate)" /p "$($SetupParameters.CodeSigningCertificatePassword)" "$($appPackageFileName.FullName)"
+        }
+        else
+        {
+            Write-Host -ForegroundColor Red "Code Signing Certificate not configured in GITSettings.json!"
+        }
     }
 }
