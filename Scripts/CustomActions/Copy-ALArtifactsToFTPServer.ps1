6299c6634b5584c6fac7ad0868b3a1e49f2de6d4
Gunnar Thor Gestsson <gunnar@navision.guru>
2019-05-14 17:14:59 +0000

    Create Copy-ALArtifactsToFTPServer.ps1

---
+++ Scripts/CustomActions/Copy-ALArtifactsToFTPServer.ps1
@@ -0,0 +1,13 @@
+﻿if ($SetupParameters.BuildMode) {
+    $version = Get-NavContainerNavVersion -containerOrImageName $BranchSettings.dockerContainerName
+    foreach ($ALPath in (Get-ChildItem -Path (Join-Path $SetupParameters.repository 'Artifacts'))) { 
+        $destFileName = ($ALPath.Name).Split(".")[0]
+        $destFileName = $destFileName.Substring(0,$destFileName.Length - 3) + ".app"
+
+        Write-Host "Uploading ${destFileName} to FTP Server..."
+        Create-FtpDirectory -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -FtpDirectoryPath "Build"
+        Create-FtpDirectory -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -FtpDirectoryPath (Join-Path "Build" $version)
+        Put-FtpFile -Server $SetupParameters.ftpServer -User $SetupParameters.ftpUser -Pass $SetupParameters.ftpPass -LocalFilePath $ALPath.FullName -FtpFilePath (Join-Path (Join-Path "Build" $version) $destFileName)
+    }
+}
+
