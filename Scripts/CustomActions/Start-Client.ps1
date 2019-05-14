c11eb16833a24328853dad87768848b622e7c9b5
Gunnar Thor Gestsson <gunnar@navision.guru>
2019-05-14 19:12:59 +0000

    Update Start-Client.ps1

--- Scripts/CustomActions/Start-Client.ps1
+++ Scripts/CustomActions/Start-Client.ps1
@@ -2,5 +2,11 @@ $ClientSettings = Prepare-NAVClient -SetupParameters $SetupParameters -BranchSet
 
 $params = @()
 $params += @('-settings:"' + $($ClientSettings.Config) + '"')
+
+if ($SetupParameters.selectClientRoleCenter) {
+    $roleCenter = Get-NAVRoleCenter -SetupParameters $SetupParameters -BranchSettings $BranchSettings
+    $params += @('-profile:"' + ${roleCenter} +'"')
+}
+
 Write-Host "Running: `"$($ClientSettings.Client)`" $params" -ForegroundColor Green
 Start-Process -FilePath $ClientSettings.Client -ArgumentList $params
