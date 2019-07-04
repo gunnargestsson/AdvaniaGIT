94959c9b770897fc8bddd6a54d558428056542ad
Gunnar Thor Gestsson <gunnar@navision.guru>
2019-07-04 11:07:27 +0000

    Update Sign-ArtifactAppPackage.ps1

--- Scripts/CustomActions/Sign-ArtifactAppPackage.ps1
+++ Scripts/CustomActions/Sign-ArtifactAppPackage.ps1
@@ -4,8 +4,9 @@
     $appPackageFileNames = Get-ChildItem -Path (Join-Path $SetupParameters.repository Artifacts) -Filter "*.app"
     foreach ($appPackageFileName in $appPackageFileNames) {
         if (Test-Path $SetupParameters.CodeSigningCertificate) {
-          Write-Host "Signing APP package $($appPackageFileName.Name)..."
-          & $($SetupParameters.SigToolExecutable) sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f "$($SetupParameters.CodeSigningCertificate)" /p "$($SetupParameters.CodeSigningCertificatePassword)" "$($appPackageFileName.FullName)"
+          $arguments = "sign /t http://timestamp.verisign.com/scripts/timestamp.dll /f $($SetupParameters.CodeSigningCertificate) /p $($SetupParameters.CodeSigningCertificatePassword) $($appPackageFileName.FullName)"
+          Write-Host "Try signing APP package $($appPackageFileName.Name)..."
+          Start-Process -FilePath $($SetupParameters.SigToolExecutable) -ArgumentList $arguments -ErrorAction SilentlyContinue -Wait
         }
         else
         {
