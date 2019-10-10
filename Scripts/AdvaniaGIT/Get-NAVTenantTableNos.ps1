19653e8c90bf1900ff54718e8bed24b41ab7b05c
Gunnar Thor Gestsson <gunnar@navision.guru>
2019-10-10 09:18:09 +0000

    Update Get-NAVTenantTableNos.ps1

--- Scripts/AdvaniaGIT/Get-NAVTenantTableNos.ps1
+++ Scripts/AdvaniaGIT/Get-NAVTenantTableNos.ps1
@@ -6,9 +6,16 @@
         [Parameter(Mandatory = $true,ValueFromPipelineByPropertyName = $true)]
         [string]$TenantDatabase,
         [Parameter(Mandatory = $false,ValueFromPipelineByPropertyName = $true)]
-        [string]$DatabaseServer = 'localhost'
+        [string]$DatabaseServer = 'localhost',
+        [Parameter(Mandatory = $false,ValueFromPipelineByPropertyName = $true)]
+        [switch]$NosOnly
+
     )
 
-    Get-SQLCommandResult -Server $DatabaseServer -Database $TenantDatabase -Command "SELECT [Object ID] As TableID, Name FROM [Object Metadata Snapshot] WHERE [Object Type] = 1 AND [Object ID] < 2000000000"
+    if ($NosOnly) {
+        Get-SQLCommandResult -Server $DatabaseServer -Database $TenantDatabase -Command "SELECT [Object ID] As TableID FROM [Object Metadata Snapshot] WHERE [Object Type] = 1 AND [Object ID] < 2000000000"
+    } else {
+        Get-SQLCommandResult -Server $DatabaseServer -Database $TenantDatabase -Command "SELECT [Object ID] As TableID, Name FROM [Object Metadata Snapshot] WHERE [Object Type] = 1 AND [Object ID] < 2000000000"
+    }
 }
 
