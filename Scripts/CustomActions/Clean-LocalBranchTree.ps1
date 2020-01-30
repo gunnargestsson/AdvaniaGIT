7c34273148b655aea3f1a2780f13b462567a48ce
Gunnar Thor Gestsson <gunnar@navision.guru>
2020-01-30 14:42:54 +0000

    Create Clean-LocalBranchTree.ps1

---
+++ Scripts/CustomActions/Clean-LocalBranchTree.ps1
@@ -0,0 +1,15 @@
+﻿$currentbranch = git.exe rev-parse --abbrev-ref HEAD
+if ($currentbranch -ne "master") {
+    Write-Error "Will only work on master branch!";
+}
+$gitstatus = git.exe status -s
+if ($gitstatus -gt '')
+{
+    Write-Error "There are uncommited changes!!!" -ErrorAction Stop
+}
+
+Write-Host "Fetch branches from GIT server..."
+git.exe fetch --prune
+
+Write-Host "Remove all local branches except master..."
+git branch | Where-Object -FilterScript {$_ -ne "* master"} | % {git branch $_.trim() -d} 
