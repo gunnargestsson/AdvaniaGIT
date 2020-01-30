95a0a76bc0a3ee7c86016989445bfb221fd8f553
Gunnar Thor Gestsson <gunnar@navision.guru>
2020-01-30 10:12:10 +0000

    Create Download-ServerAddins.ps1

---
+++ Scripts/CustomActions/Download-ServerAddins.ps1
@@ -0,0 +1,19 @@
+﻿if ($BranchSettings.dockerContainerId -gt "") {
+    Start-DockerCustomAction -BranchSettings $BranchSettings -ScriptName $MyInvocation.MyCommand.Name -BuildSettings $BuildSettings    
+}
+
+$SetupParameters
+$FileName = Join-Path $SetupParameters.LogPath 'AddIns.zip'
+if ($SetupParameters.addinsUrl) {
+    Download-NAVFile -Url $SetupParameters.addinsUrl -FileName $FileName
+} else {
+    Download-NAVFile -Url "https://addins.navleiga.is/addins.zip" -FileName $FileName
+}
+
+if ([String]::IsNullOrEmpty($SetupParameters.navServicePath)) {
+    $AddInsPath = Join-Path (Get-NAVServicePath -SetupParameters $SetupParameters -ErrorIfNotFound) "Add-ins\AdvaniaGIT"
+} else  {
+    $AddInsPath = Join-Path $SetupParameters.navServicePath "Add-ins\AdvaniaGIT"
+}
+New-Item -Path $AddInsPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+Expand-Archive -LiteralPath $FileName -DestinationPath $AddInsPath -Force
