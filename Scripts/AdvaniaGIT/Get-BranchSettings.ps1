0d6ba50890d6a12868d0ac0ce2e2b6cd4dad85ec
Gunnar Thor Gestsson <gunnar@navision.guru>
2020-05-26 11:46:05 +0000

    Change access to brancsettings.json

--- Scripts/AdvaniaGIT/Get-BranchSettings.ps1
+++ Scripts/AdvaniaGIT/Get-BranchSettings.ps1
@@ -6,7 +6,17 @@
         [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
         [String]$SettingsFilePath = (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot))  "Data\BranchSettings.Json")
     )
-    $allBranchSettings = Get-Content -Path $SettingsFilePath | Out-String | ConvertFrom-Json
+    $file = $null
+    while (!($file)) {
+        try {
+            $file = [System.IO.File]::Open($SettingsFilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::Read)
+        } catch [System.IO.IOException]  {
+            Start-Sleep -Seconds 1
+        }
+    }
+    $content = New-Object System.Byte[] ($file.Length)
+    $file.Read($content, 0, $file.Length) | Out-Null    
+    $allBranchSettings = [System.Text.Encoding]::UTF8.GetString($content) | Out-String | ConvertFrom-Json    
     if (![String]::IsNullOrEmpty($SetupParameters.dockerShared) -and !$SetupParameters.BuildMode) {
         $branchSettings = ($allBranchSettings.Branches | Where-Object -Property dockerContainerName -EQ $SetupParameters.projectName)
     } else {
@@ -28,7 +38,11 @@
             "dockerContainerId" = "";
             "dockerContainerIp" = "";}
         $allBranchSettings.Branches += $BranchSettings
-        Set-Content -Path $SettingsFilePath -Value ($allBranchSettings | ConvertTo-Json)        
+        $content = [System.Text.Encoding]::UTF8.GetBytes(($allBranchSettings | ConvertTo-Json))
+        $file.Seek(0, [System.IO.SeekOrigin]::Begin) | Out-Null
+        $file.Write($content, 0, $content.Length)
+        $file.SetLength($content.Length)
+        $file.Flush()        
     } else {
         if (![bool]($BranchSettings.PSObject.Properties.name -match "dockerContainerName")) {
             $BranchSettings | Add-Member -MemberType NoteProperty -Name dockerContainerName -Value ""
@@ -46,5 +60,6 @@
             $BranchSettings | Add-Member -MemberType NoteProperty -Name instanceServer -Value "localhost"
         }
     }
+    $file.Dispose()
     Return $BranchSettings
 }
--- Scripts/AdvaniaGIT/Update-BranchSettings.ps1
+++ Scripts/AdvaniaGIT/Update-BranchSettings.ps1
@@ -4,12 +4,29 @@
         [Parameter(Mandatory=$True, ValueFromPipelineByPropertyname=$true)]
         [PSObject]$BranchSettings,
         [Parameter(Mandatory=$False, ValueFromPipelineByPropertyname=$true)]
-        [String]$SettingsFilePath = (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) "Data\BranchSettings.Json")
-    ) 
-    $allBranchSettings = Get-Content -Path $SettingsFilePath | Out-String | ConvertFrom-Json
+        [String]$SettingsFilePath = (Join-Path (Split-Path -Parent (Split-Path -Parent $PSScriptRoot)) "Data\BranchSettings.Json"),
+        [Parameter(Mandatory=$False)]
+        [Switch]$Clear
+    )
+    $file = $null
+    while (!($file)) {
+        try {
+            $file = [System.IO.File]::Open($SettingsFilePath, [System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::Read)
+        } catch [System.IO.IOException]  {
+            Start-Sleep -Seconds 1
+        }
+    }
+    $content = New-Object System.Byte[] ($file.Length)
+    $file.Read($content, 0, $file.Length) | Out-Null    
+    $allBranchSettings = [System.Text.Encoding]::UTF8.GetString($content) | Out-String | ConvertFrom-Json
     $newBranchSettings = @()
     $allBranchSettings.Branches | Where-Object -Property branchId -NE $BranchSettings.branchId | foreach {$newBranchSettings += $_}
-    $newBranchSettings += $BranchSettings
+    if (!$Clear) {$newBranchSettings += $BranchSettings}
     $allBranchSettings.Branches = $newBranchSettings
-    Set-Content -Path $SettingsFilePath -Value ($allBranchSettings | ConvertTo-Json)             
+    $content = [System.Text.Encoding]::UTF8.GetBytes(($allBranchSettings | ConvertTo-Json))
+    $file.Seek(0, [System.IO.SeekOrigin]::Begin) | Out-Null
+    $file.Write($content, 0, $content.Length)
+    $file.SetLength($content.Length)
+    $file.Flush()
+    $file.Dispose()    
 }
