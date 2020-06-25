318776e2a614d1bb291224d7f3a97b9e5770a692
Gunnar Thor Gestsson <gunnar@navision.guru>
2020-06-25 14:40:58 +0000

    Update remote

--- Scripts/RemoteManagement/Get-NAVADApplication.ps1
+++ Scripts/RemoteManagement/Get-NAVADApplication.ps1
@@ -14,7 +14,7 @@
     PROCESS 
     {    
         $DisplayName = "${DeploymentName}-$($ServerInstance.ServerInstance)"
-        $Application = Get-AzureRmADApplication -DisplayNameStartWith $DisplayName        
+        $Application = Get-AzureRmADApplication -DisplayName $DisplayName        
         if (!$Application) {
             $ReplaceApplication = $true
         } else {
@@ -46,8 +46,8 @@
             $Application = Get-AzureRmADApplication -DisplayNameStartWith $DisplayName
         } else {
             $x509 = [System.Security.Cryptography.X509Certificates.X509Certificate2]([System.Convert]::FromBase64String($CertValue))
-            Get-AzureRmADAppCredential -ObjectId $Application.ObjectId | Remove-AzureRmADAppCredential -ObjectId $Application.ObjectId -Force | Out-Null
-            $newCredential = New-AzureRmADAppCredential -ObjectId $Application.ObjectId -CertValue $CertValue -StartDate $x509.NotBefore -EndDate $x509.NotAfter
+            Get-AzureRmADAppCredential -ObjectId $Application.ObjectId | Remove-AzureRmADAppCredential -ObjectId $Application.ObjectId -Force -ErrorAction SilentlyContinue
+            $newCredential = New-AzureRmADAppCredential -ObjectId $Application.ObjectId -CertValue $CertValue -StartDate $x509.NotBefore -EndDate $x509.NotAfter -ErrorAction SilentlyContinue
         }
         Return $Application
     }
