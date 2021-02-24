d68acf90d3072aed957b3d6110d4c0a465e8fa28
Henrik Helgesen <henrik@helgesen.consulting>
2021-02-23 21:14:52 -0800

    Combine Test Objects into one file before importing

--- Scripts/CustomActions/ImportFrom-GITTestObjectsToNAV.ps1
+++ Scripts/CustomActions/ImportFrom-GITTestObjectsToNAV.ps1
@@ -6,10 +6,22 @@
 # Import Custom Test objects
 if (Test-Path $SetupParameters.testObjectsPath) {
     Load-ModelTools -SetupParameters $SetupParameters
+
+    $CombinedTestFileName = $SetupParameters.workFolder + "\TestObjects.txt"
+        Write-Host("CombinedFileName value: $($CombinedTestFileName)")
+        New-Item -ItemType file $CombinedTestFileName –force
+
     foreach ($testObjectFile in (Get-ChildItem -Path (Join-Path $SetupParameters.testObjectsPath '*.txt'))) {
-        Write-Host "Importing $($testObjectFile.Name)..."
-        Update-NAVApplicationFromTxt -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $testObjectFile.FullName -SkipDeleteCheck
+        #Write-Host "Importing $($testObjectFile.Name)..."
+        #Update-NAVApplicationFromTxt -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $testObjectFile.FullName -SkipDeleteCheck
+        ##Write-Verbose -Message "Adding FileName.FileName :. $($testObjectFile.FileName.FileName)"
+        Write-Verbose -Message "Adding Name :  $($testObjectFile.Name)"
+        Get-Content $($testObjectFile.FullName) | Add-Content $CombinedTestFileName
     }
+    
+    Write-Host "Importing $($CombinedTestFileName)..."
+    Update-NAVApplicationFromTxt -SetupParameters $SetupParameters -BranchSettings $BranchSettings -ObjectsPath $CombinedTestFileName -SkipDeleteCheck
+        
     foreach ($testObjectFile in (Get-ChildItem -Path (Join-Path $SetupParameters.testObjectsPath '*.fob'))) {
         Write-Host "Importing $($testObjectFile.Name)..."
         Import-NAVApplicationGITObject -SetupParameters $SetupParameters -BranchSettings $BranchSettings -Path $testObjectFile.FullName -ImportAction Overwrite -SynchronizeSchemaChanges Force
@@ -21,6 +33,3 @@ if (Test-Path $SetupParameters.testObjectsPath) {
     & (Join-Path $PSScriptRoot 'Start-ForceSync.ps1')
     UnLoad-ModelTools
 }
-
-        
-
